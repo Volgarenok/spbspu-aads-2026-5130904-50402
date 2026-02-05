2ff762bd2e4fc23e0311d2d7502861896a1e6c3e
lavrentev.makar <makar.lavrentev@yandex.ru>
2026-02-05 12:46:34 +0300

    added test-main

--- lavrentev.makar/S0/test-credentials.cpp
+++ lavrentev.makar/S0/test-credentials.cpp
@@ -1,3 +1,6 @@
+#define BOOST_TEST_DYN_LINK 
+#define BOOST_TEST_MODULE CredentialsTest
+
 #include <boost/test/unit_test.hpp>
 #include <sstream>
 #include "credentials.hpp"
@@ -7,4 +10,4 @@ BOOST_AUTO_TEST_CASE(credentials_test)
     std::ostringstream out;
     lavrentev::out_credentials(out);
     BOOST_TEST(out.str() == "lavrentev.makar");
-}
+};
