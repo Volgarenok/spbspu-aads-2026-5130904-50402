d77a39d1cc1672968d9be482d27f463258d44d75
voyadger01 <voyadgerkx@gmail.com>
2026-02-28 23:16:21 +0300

    iterators declaration

--- karpovich.dmitriy/S1/iterators.hpp
+++ karpovich.dmitriy/S1/iterators.hpp
@@ -6,10 +6,24 @@ namespace karpovich
   template< class T >
   class LIter {
     friend class List< T >;
+    Node* ptr;
+  public:
+    T& operator*() const;
+    LIter& operator++();
+    LIter& operator--();
+    LIter& operator==();
+    LIter& operator!=();
   };
   template< class T >
   class LCIter {
     friend class List< T >;
+    const Node* ptr;
+  public:
+    T& operator*() const;
+    LCIter& operator++();
+    LCIter& operator--();
+    LCIter& operator==();
+    LCIter& operator!=();
   };
 }
 #endif
--- karpovich.dmitriy/S1/list.hpp
+++ karpovich.dmitriy/S1/list.hpp
@@ -3,7 +3,7 @@
 #include "node.hpp"
 namespace karpovich
 {
-  template< class T > 
+  template< class T >
   class List {
     Node* head;
     Node* tail;
@@ -14,6 +14,18 @@ namespace karpovich
     List& operator=(const List< T >&);
     List& operator=(List< T >&&);
     ~List();
+
+    LIter< T > begin();
+    LIter< T > end();
+    LCIter< T > begin() const;
+    LCIter< T > end() const;
+
+    void push_front(const T&);
+    void push_back(const T&);
+    void pop_front();
+    void pop_back();
+    void clear();
+    size_t size() const;
   };
 }
 #endif
