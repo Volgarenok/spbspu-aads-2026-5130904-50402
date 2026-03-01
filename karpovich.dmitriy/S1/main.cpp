8dbbc408dc88d343f51ad2f784a3a86771989a7c
voyadger01 <voyadgerkx@gmail.com>
2026-03-01 18:50:29 +0300

    add main

--- karpovich.dmitriy/S1/main.cpp
+++ karpovich.dmitriy/S1/main.cpp
@@ -3,5 +3,15 @@
 int main()
 {
   namespace karp = karpovich;
-  
+  karp::List< std::pair< std::string, karp::List< size_t > > > sequences;
+  karp::List< karp::List< size_t > > transposed;
+  karp::input(std::cin, sequences);
+
+  if (sequences.size() == 0) {
+    std::cout << "0\n";
+    return 0;
+  }
+
+  karp::transpose(sequences, transposed);
+  karp::output(std::cout, sequences, transposed);
 }
