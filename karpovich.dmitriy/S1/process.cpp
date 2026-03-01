8fbdfef876f330169e846ccbf545ab92083ebe0a
voyadger01 <voyadgerkx@gmail.com>
2026-03-01 16:24:14 +0300

    fix

--- karpovich.dmitriy/S1/process.cpp
+++ karpovich.dmitriy/S1/process.cpp
@@ -6,7 +6,7 @@ void karpovich::input(std::istream& in, List< std::pair< std::string, List< size
   while (in >> name) {
     List< size_t > numbers;
     size_t num = 0;
-    while (in.peek() != '\n' && in.peek() != in.eof()) {
+    while (in.peek() != '\n' && !in.eof()) {
       if (in >> num) {
         numbers.push_back(num);
       } else {
@@ -18,4 +18,11 @@ void karpovich::input(std::istream& in, List< std::pair< std::string, List< size
     }    
     list.push_back(std::make_pair(name, numbers));
   }
+}
+
+
+
+void karpovich::output(std::ostream& out)
+{
+
 }
