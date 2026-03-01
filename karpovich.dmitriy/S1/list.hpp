82d321f72794770681598bdeaf4b0e1aa110fa32
voyadger01 <voyadgerkx@gmail.com>
2026-03-01 14:15:14 +0300

    fixes

--- karpovich.dmitriy/S1/list.hpp
+++ karpovich.dmitriy/S1/list.hpp
@@ -4,16 +4,17 @@
 #include "iterators.hpp"
 namespace karpovich
 {
-  template< class T >
-  class List {
-    Node* fake_;
+  template < class T > class List
+  {
+    Node< T > *fake_;
     size_t size_;
+
   public:
     List();
-    List(const List< T >& other);
-    List(List< T >&& other) noexcept;
-    List& operator=(const List< T >& other);
-    List& operator=(List< T >&& other) noexcept;
+    List(const List< T > &other);
+    List(List< T > &&other) noexcept;
+    List &operator=(const List< T > &other);
+    List &operator=(List< T > &&other) noexcept;
     ~List();
 
     LIter< T > begin();
@@ -21,16 +22,15 @@ namespace karpovich
     LCIter< T > begin() const;
     LCIter< T > end() const;
 
-    void push_front(const T&);
-    void push_back(const T&);
+    void push_front(const T &);
+    void push_back(const T &);
     void pop_front();
     void pop_back();
     void clear();
     size_t size() const;
   };
 
-
-  template< class T >
+  template < class T >
   List< T >::List():
     fake_(new Node< T >{T(), nullptr, nullptr}),
     size_(0)
@@ -39,37 +39,36 @@ namespace karpovich
     fake_->prev = fake_;
   }
 
-  template< class T >
-  List< T >::List(const List< T >& other): 
-    size_(0), 
-    fake_(new Node<T>{T(), nullptr, nullptr}) 
+  template < class T >
+  List< T >::List(const List< T > &other):
+    size_(0),
+    fake_(new Node< T >{T(), nullptr, nullptr})
   {
     fake_->next = fake_;
     fake_->prev = fake_;
-    Node<T>* cur = other.fake_->next;
+    Node< T > *cur = other.fake_->next;
     while (cur != other.fake_) {
       push_back(cur->val);
       cur = cur->next;
     }
   }
 
-  template< class T >
-  List< T >::List(List< T >&& other) noexcept:
+  template < class T >
+  List< T >::List(List< T > &&other) noexcept:
     fake_(other.fake_),
     size_(other.size_)
   {
-    other.fake_ = new Node<T>{T(), nullptr, nullptr};
+    other.fake_ = new Node< T >{T(), nullptr, nullptr};
     other.fake_->next = other.fake_;
     other.fake_->prev = other.fake_;
     other.size_ = 0;
   }
 
-  template< class T >
-  List< T >& List< T >::operator=(const List< T >& other)
+  template < class T > List< T > &List< T >::operator=(const List< T > &other)
   {
     if (this != &other) {
       clear();
-      Node* cur = other.fake_->next;
+      Node< T > *cur = other.fake_->next;
       while (cur != other.fake_) {
         push_back(cur->val);
         cur = cur->next;
@@ -78,22 +77,25 @@ namespace karpovich
     return *this;
   }
 
-  template< class T >
-  List< T >& List< T >::operator=(List< T >&& other) noexcept
+  template < class T > List< T > &List< T >::operator=(List< T > &&other) noexcept
   {
     if (this != &other) {
       clear();
+      delete fake_;
       fake_ = other.fake_;
       size_ = other.size_;
-      other.fake_ = nullptr;
+      other.fake_ = new Node< T >{T(), nullptr, nullptr};
+      other.fake_->next = other.fake_;
+      other.fake_->prev = other.fake_;
+      other.size_ = 0;
     }
     return *this;
   }
 
-  template< class T >
-  List< T >::~List()
+  template < class T > List< T >::~List()
   {
     clear();
+    delete fake_;
   }
 }
 #endif
