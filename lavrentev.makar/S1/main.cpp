835da49c92581c5e1467dda97c3ef6adfcca651e
lavrentev.makar <makar.lavrentev@yandex.ru>
2026-03-04 18:58:43 +0300

    Added namespace with class, iters and initialization of getline func

--- lavrentev.makar/S1/main.cpp
+++ lavrentev.makar/S1/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
+namespace lavrentev
+{
+  template <class T> class List
+  {
+    T val;
+    List<T> *next;
+  };
+
+  template <class T> class LIter
+  {
+    friend class List<T>;
+    List<T> *curr;
+  };
+
+  template <class T> class LCIter
+  {
+    friend class List<T>;
+    const List<const T> *curr;
+  };
+
+  std::pair<std::string, List<int>> getline(std::istream &in, size_t &n);
+}
+
 int main()
 {
-
 }
