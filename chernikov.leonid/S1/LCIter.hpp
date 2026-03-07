b21dc98b7905d89a23f0b7ddd10cfafd331df360
martin0martinase <leonidcernikov43@gmail.com>
2026-03-07 16:19:45 +0300

    Iterators classes init

---
+++ chernikov.leonid/S1/LCIter.hpp
@@ -0,0 +1,56 @@
+#ifndef LCITER_HPP
+#define LCITER_HPP
+
+#include "vanila_list.hpp"
+#include "node.hpp"
+
+namespace petrov
+{
+
+  template < typename T > class LCIter
+  {
+    friend class List< T >;
+
+  private:
+    const Node< T > *ptr;
+
+    explicit LCIter(const Node< T > *p):
+      ptr(p)
+    {}
+
+  public:
+    LCIter():
+      ptr(nullptr)
+    {}
+    LCIter(const LIter< T > &it):
+      ptr(it.ptr)
+    {}
+
+    bool operator==(const LCIter &other) const
+    {
+      return ptr == other.ptr;
+    }
+    bool operator!=(const LCIter &other) const
+    {
+      return ptr != other.ptr;
+    }
+
+    const T &operator*() const
+    {
+      return ptr->data;
+    }
+    const T *operator->() const
+    {
+      return &ptr->data;
+    }
+
+    LCIter &operator++()
+    {
+      ptr = ptr->next;
+      return *this;
+    }
+  };
+
+}
+
+#endif
---
+++ chernikov.leonid/S1/LIter.hpp
@@ -0,0 +1,51 @@
+#ifndef LITER_HPP
+#define LITER_HPP
+
+#include "vanila_list.hpp"
+#include "node.hpp"
+
+namespace chernikov
+{
+
+  template < typename T > class LIter
+  {
+    friend class List< T >;
+
+  private:
+    Node< T > *ptr;
+
+    explicit LIter(Node< T > *p):
+      ptr(p)
+    {}
+
+  public:
+    LIter():
+      ptr(nullptr)
+    {}
+
+    bool operator==(const LIter &other) const
+    {
+      return ptr == other.ptr;
+    }
+    bool operator!=(const LIter &other) const
+    {
+      return ptr != other.ptr;
+    }
+
+    T &operator*()
+    {
+      return ptr->data;
+    }
+    T *operator->()
+    {
+      return &ptr->data;
+    }
+
+    LIter &operator++()
+    {
+      ptr = ptr->next;
+      return *this;
+    }
+  };
+}
+#endif
