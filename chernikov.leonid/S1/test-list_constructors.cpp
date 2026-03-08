d67a997b435da10250bf4053f805fe5b5e6b8954
martin0martinase <leonidcernikov43@gmail.com>
2026-03-08 15:18:27 +0300

    move constructor added

--- chernikov.leonid/S1/test-list_constructors.cpp
+++ chernikov.leonid/S1/test-list_constructors.cpp
@@ -15,10 +15,26 @@ namespace chernikov {
   BOOST_AUTO_TEST_CASE(copy_costructor)
   {
     List< int > original;
+    original.add(42);
+    original.add(13);
     List< int > copy(original);
     auto orig_it = original.begin();
     auto cp_it = copy.begin();
-    BOOST_TEST(*orig_it == *cp_it);
+    BOOST_TEST(*cp_it == *orig_it);
+    ++cp_it;
+    ++orig_it;
+    BOOST_TEST(*cp_it == *orig_it);
+  }
+  BOOST_AUTO_TEST_CASE(move_constructor_transfers_ownership)
+  {
+    List< int > original;
+    original.add(42);
+
+    List< int > moved(std::move(original));
+
+    BOOST_TEST(moved.size() == 1);
+    BOOST_TEST(*moved.begin() == 42);
+    BOOST_TEST(original.empty());
   }
   BOOST_AUTO_TEST_SUITE_END()
 
