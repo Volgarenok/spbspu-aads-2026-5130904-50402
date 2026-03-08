97c3f260609fbb36d9b938e9c61b0be144dcd9c4
Jaroslaw-super1 <Jarafan3@gmail.com>
2026-03-08 17:11:50 +0500

    add class name

--- afanasev.yaroslav/S1/main.cpp
+++ afanasev.yaroslav/S1/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+
+namespace afanasev
+{
+  template< class T > class List;
+  template< class T > class LIter;
+  template< class T > class LCIter;
+}
+
 int main()
 {
   return 0;
