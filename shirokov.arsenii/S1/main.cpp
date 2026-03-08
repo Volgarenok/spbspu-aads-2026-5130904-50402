3585c632422c164a28f5fe10cc618143b3b1d3da
s1nhronn <silekatik@yandex.ru>
2026-03-08 21:22:05 +0300

    Implementation of the main function

--- shirokov.arsenii/S1/main.cpp
+++ shirokov.arsenii/S1/main.cpp
@@ -1,3 +1,111 @@
+#include <iostream>
+#include "bilist.hpp"
+
+namespace shirokov
+{
+  struct BLEnds
+  {
+    shirokov::BLIter< int > curr;
+    shirokov::BLIter< int > end;
+  };
+}
+
 int main()
 {
+  auto sequences = shirokov::BiList< std::pair< std::string, shirokov::BiList< int > > >();
+
+  {
+    std::pair< std::string, shirokov::BiList< int > > pair;
+    std::string name;
+    auto seq = shirokov::BiList< int >();
+
+    int curr = 0;
+    std::cin >> name;
+
+    while (true)
+    {
+      if (!(std::cin >> curr))
+      {
+        if (std::cin.eof())
+        {
+          pair = {name, seq};
+          sequences.push_back(pair);
+          break;
+        }
+        std::cin.clear();
+        std::string bad;
+        if (std::cin >> bad)
+        {
+          pair = {name, seq};
+          sequences.push_back(pair);
+          seq.clear();
+          name = bad;
+        }
+      }
+      else
+      {
+        seq.push_back(curr);
+      }
+    }
+  }
+
+  for (const auto& pair : sequences)
+  {
+    if (&pair == &sequences.back())
+    {
+      std::cout << pair.first << '\n';
+    }
+    else
+    {
+      std::cout << pair.first << ' ';
+    }
+  }
+
+  auto listOfSum = shirokov::BiList< int >();
+  shirokov::BiList< shirokov::BLEnds > iterators;
+  for (auto& pair : sequences)
+  {
+    iterators.push_back({pair.second.begin(), pair.second.end()});
+  }
+
+  while (true)
+  {
+    bool flag = true;
+    int sum = 0;
+    for (shirokov::BLEnds& ends : iterators)
+    {
+      if (ends.curr == ends.end)
+      {
+        continue;
+      }
+      if (&ends != &iterators.front())
+      {
+        std::cout << ' ';
+      }
+      std::cout << *ends.curr;
+      sum += *ends.curr;
+      ++ends.curr;
+      flag = false;
+    }
+    if (flag)
+    {
+      break;
+    }
+    std::cout << '\n';
+    listOfSum.push_back(sum);
+  }
+
+  if (listOfSum.empty())
+  {
+    std::cout << 0 << '\n';
+  }
+  for (int s : listOfSum)
+  {
+    if (s != listOfSum.front())
+    {
+      std::cout << ' ';
+    }
+    std::cout << s;
+  }
+  std::cout << '\n';
 }
