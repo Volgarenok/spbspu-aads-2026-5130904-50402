e8fdc600996ec0f7fad7e6f04978df4e850b8170
Jaroslaw-super1 <Jarafan3@gmail.com>
2026-03-11 00:19:37 +0500

    fix output

--- afanasev.yaroslav/S1/main.cpp
+++ afanasev.yaroslav/S1/main.cpp
@@ -501,7 +501,7 @@ int main()
   }
   catch (const std::overflow_error & e)
   {
-    std::cerr << e.what() << std::endl;
+    std::cerr << '\n' << e.what() << '\n';
     return 1;
   }
   return 0;
