ced1d16a938c50f26d63a4f30275dda443755813
khalii21 <miratkhalikov007@gmail.com>
2026-03-11 19:40:39 +0500

    full header of list

--- khalikov.mirat/S1/list.hpp
+++ khalikov.mirat/S1/list.hpp
@@ -19,6 +19,19 @@ namespace khalikov
     ~List();
     List< T > & operator=(const List< T > & h);
     List(const List< T > & other);
+    LIter< T > begin();
+    LCIter< T > cbegin() const;
+    bool empty() const;
+    void erase_after(LIter< T > pos);
+    void insert_after(LIter< T > pos, const T & value);
+    void merge(List< T > & other);
+    void pop_front();
+    void push_front(const T & value);
+    void remove(const T & value);
+    void sort();
+    void unique();
+    void reverse();
+    void print(const List< T > curr);
   };
 }
 
