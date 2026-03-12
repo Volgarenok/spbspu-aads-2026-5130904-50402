af570b6ac5ccfaa37be14e920c650be430b06b9e
Carrot_lord <lev.saldaev@gmai.com>
2026-03-12 22:48:12 +0300

    Implement sequence reading logic

--- saldaev.lev/S1/main.cpp
+++ saldaev.lev/S1/main.cpp
@@ -1,6 +1,22 @@
+#include "List.hpp"
 #include <iostream>
 #include <string>
-#include "List.hpp"
 
 int main()
-{}
+{
+  saldaev::List< std::pair< std::string, saldaev::List< int > > > outer;
+
+  std::string name = "";
+  size_t num = 0;
+  while (std::cin >> name) {
+    saldaev::List< int > numList;
+    while (std::cin >> num) {
+      numList.newTail(num);
+    }
+    outer.newTail({name, std::move(numList)});
+
+    if (!std::cin.eof()) {
+      std::cin.clear();
+    }
+  }
+}
