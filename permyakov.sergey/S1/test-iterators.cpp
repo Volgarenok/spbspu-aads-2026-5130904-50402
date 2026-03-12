e04c44295209354aa0d65e3a586e703866b9d0f9
Stave143 <staveplay@mail.ru>
2026-03-12 21:05:22 +0300

    init test

---
+++ permyakov.sergey/S1/test-iterators.cpp
@@ -0,0 +1,6 @@
+#include <boost/test/unit_test.hpp>
+#include "iterators.hpp"
+
+using namespace permyakov;
+
+BOOST_AUTO_TEST_SUITE(IteratorsMethodsTests)
---
+++ permyakov.sergey/S1/test-list.hpp
@@ -0,0 +1,7 @@
+#include <boost/test/unit_test.hpp>
+#include "iterators.hpp"
+#include "list.hpp"
+
+using namespace permyakov;
+
+BOOST_AUTO_TEST_SUITE(ListMethodsTests)
