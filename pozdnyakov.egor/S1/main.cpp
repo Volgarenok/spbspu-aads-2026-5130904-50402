2071c46f811c159b7b48e16d7148a561744a3389
R0me0Star <eip_spb@mail.ru>
2026-03-14 00:33:11 +0300

    logic implementation

--- pozdnyakov.egor/S1/main.cpp
+++ pozdnyakov.egor/S1/main.cpp
@@ -183,4 +183,95 @@ namespace pozdnyakov {
     LCIter<T> cend() const { return LCIter<T>(nullptr); }
   };
 
+  using ValueType = unsigned long long;
+  using NamedSequence = std::pair<std::string, List<ValueType>>;
+
+  struct ProcessResult {
+    List<List<ValueType>> rows;
+    List<ValueType> sums;
+  };
+
+  ProcessResult build_interleaved_rows(const List<NamedSequence>& sequences) {
+    ProcessResult result;
+
+    List<LCIter<ValueType>> iters;
+    List<LCIter<ValueType>> ends;
+
+    LIter<LCIter<ValueType>> iters_tail = iters.end();
+    LIter<LCIter<ValueType>> ends_tail = ends.end();
+
+    for (auto it = sequences.cbegin(); it != sequences.cend(); ++it) {
+      if (iters.empty()) {
+        iters.push_front((*it).second.cbegin());
+        ends.push_front((*it).second.cend());
+        iters_tail = iters.begin();
+        ends_tail = ends.begin();
+      }
+      else {
+        iters.insert_after(iters_tail, (*it).second.cbegin());
+        ends.insert_after(ends_tail, (*it).second.cend());
+        ++iters_tail;
+        ++ends_tail;
+      }
+    }
+
+    LIter<List<ValueType>> rows_tail = result.rows.end();
+    LIter<ValueType> sums_tail = result.sums.end();
+
+    while (true) {
+      bool elements_left = false;
+      ValueType current_row_sum = 0;
+
+      List<ValueType> current_row;
+      LIter<ValueType> current_row_tail = current_row.end();
+
+      auto it = iters.begin();
+      auto end_it = ends.begin();
+
+      for (; it != iters.end() && end_it != ends.end(); ++it, ++end_it) {
+        if (*it != *end_it) {
+          elements_left = true;
+          ValueType val = *(*it);
+
+          if (current_row.empty()) {
+            current_row.push_front(val);
+            current_row_tail = current_row.begin();
+          }
+          else {
+            current_row.insert_after(current_row_tail, val);
+            ++current_row_tail;
+          }
+
+          if (current_row_sum > std::numeric_limits<ValueType>::max() - val) {
+            throw std::overflow_error("Произошло переполнение при подсчете суммы.");
+          }
+          current_row_sum += val;
+          ++(*it);
+        }
+      }
+
+      if (!elements_left) break;
+
+      if (result.rows.empty()) {
+        result.rows.push_front(current_row);
+        rows_tail = result.rows.begin();
+      }
+      else {
+        result.rows.insert_after(rows_tail, current_row);
+        ++rows_tail;
+      }
+
+      if (result.sums.empty()) {
+        result.sums.push_front(current_row_sum);
+        sums_tail = result.sums.begin();
+      }
+      else {
+        result.sums.insert_after(sums_tail, current_row_sum);
+        ++sums_tail;
+      }
+    }
+
+    return result;
+  }
+
 }
