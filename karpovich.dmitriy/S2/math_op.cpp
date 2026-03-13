88d6aa68aec90a4fe8cf5006d0a843b518e87004
voyadger01 <voyadgerkx@gmail.com>
2026-03-13 23:32:26 +0300

    add operations

--- karpovich.dmitriy/S2/math_op.cpp
+++ karpovich.dmitriy/S2/math_op.cpp
@@ -1,1 +1,51 @@
 #include "math_op.hpp"
+#include <climits>
+#include <stdexcept>
+
+long long karpovich::sum(long long a, long long b)
+{
+  if ((b > 0 && a > LLONG_MAX - b) || (b < 0 && a < LLONG_MIN - b)) {
+    throw std::runtime_error("Overflow");
+  }
+  return a + b;
+}
+long long karpovich::divide(long long a, long long b)
+{
+  if (b == 0) {
+    throw std::runtime_error("Division by zero");
+  }
+  return a / b;
+}
+long long karpovich::subtract(long long a, long long b)
+{
+  if ((b > 0 && a < LLONG_MIN + b) || (b < 0 && a > LLONG_MAX + b)) {
+    throw std::runtime_error("Overflow");
+  }
+  return a - b;
+}
+long long karpovich::mod(long long a, long long b)
+{
+  if (b == 0) {
+    throw std::runtime_error("Division by zero");
+  }
+  return a % b;
+}
+long long karpovich::multiply(long long a, long long b)
+{
+  if (a > 0) {
+    if (b > 0 && a > LLONG_MAX / b) {
+      throw std::runtime_error("Overflow");
+    }
+    if (b < 0 && b < LLONG_MIN / a) {
+      throw std::runtime_error("Overflow");
+    }
+  } else {
+    if (b > 0 && a < LLONG_MIN / b) {
+      throw std::runtime_error("Overflow");
+    }
+    if (b < 0 && a < LLONG_MAX / b) {
+      throw std::runtime_error("Overflow");
+    }
+  }
+  return a * b;
+}
--- karpovich.dmitriy/S2/math_op.hpp
+++ karpovich.dmitriy/S2/math_op.hpp
@@ -3,6 +3,10 @@
 
 namespace karpovich
 {
-
+  long long sum(long long a, long long b);
+  long long divide(long long a, long long b);
+  long long subtract(long long a, long long b);
+  long long mod(long long a, long long b);
+  long long multiply(long long a, long long b);
 }
 #endif
