21026dba9630a404db89360bb5d547bf4cf874a6
sp0146001 <sp0146001@gmail.com>
2026-03-13 22:17:19 +0300

    main logic

--- petrov.sasha/S1/main.cpp
+++ petrov.sasha/S1/main.cpp
@@ -4,9 +4,6 @@
 #include <string>
 #include <utility>
 
-int main()
-{}
-
 bool petrov::readSequences(std::istream& input, SequenceList& sequences) {
   while (true) {
     std::string name = "";
@@ -98,4 +95,29 @@ void petrov::printNames(std::ostream& output, const SequenceList& sequences) {
     ++it;
   }
   output << '\n';
+}
+
+int main() {
+  petrov::SequenceList sequences;
+
+  if (!petrov::readSequences(std::cin, sequences)) {
+    std::cerr << "Input error\n";
+    return 1;
+  }
+  if (sequences.empty()) {
+    std::cout << "0\n";
+    return 0;
+  }
+
+  petrov::printNames(std::cout, sequences);
+  petrov::RowList rows;
+  petrov::buildRows(sequences, rows);
+  petrov::printRows(std::cout, rows);
+  petrov::NumberList sums;
+  if (!petrov::buildSums(rows, sums)) {
+    std::cerr << "Sum overflow\n";
+    return 1;
+  }
+  petrov::printSums(std::cout, sums);
+  return 0;
 }
