8091538c671a197cb96da8a7e431e2d38ade9bd4
sp0146001 <sp0146001@gmail.com>
2026-03-13 21:12:12 +0300

    read sequences

--- petrov.sasha/S1/main.cpp
+++ petrov.sasha/S1/main.cpp
@@ -1,5 +1,42 @@
-#include <iostream>
+#include "list.hpp"
 
-int main() {
-	std::cout << "Initial\n";
+#include <iostream>
+#include <string>
+#include <utility>
+
+int main()
+{}
+
+bool petrov::readSequences(std::istream& input, SequenceList& sequences) {
+  while (true) {
+    std::string name = '';
+    if (!(input >> name)) {
+      if (input.eof()) {
+        return true;
+      }
+      return false;
+    }
+    NumberList numbers;
+    while(true) {
+      const int next_symbol = input.peek();
+      if (next_symbol == '\n') {
+        input.get();
+        break;
+      }
+      if (next_symbol == std::char_traits< char >::eof()) {
+        break;
+      }
+      std::size_t number = 0;
+      if (!(input >> number)) {
+        return false;
+      }
+      numbers.pushBack(number);
+    }
+    sequences.pushBack(std::make_pair(name, std::move(numbers)));
+
+    if (input.eof()) {
+      break;
+    }
+  }
+  return true;
 }
