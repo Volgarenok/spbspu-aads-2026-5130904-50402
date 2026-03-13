747b2925c397c6b9f36adfd4241b3d05d5055d01
sp0146001 <sp0146001@gmail.com>
2026-03-13 20:19:36 +0300

    Finish list.hpp file with full realize this logic of task Bidir

--- petrov.sasha/S1/list.hpp
+++ petrov.sasha/S1/list.hpp
@@ -248,6 +248,20 @@ namespace petrov
     Node< T >* tail_;
     std::size_t size_;
   };
+
+  using NumberList = List< std::size_t >;
+  using NamedSequence = std::pair< std::string, NumberList >;
+  using SequenceList = List< NamedSequence >;
+  using RowList = List< NumberList >;
+
+  bool readSequences(std::istream & input, SequenceList & sequences);
+  std::size_t getMaxLength(const SequenceList & sequences);
+  void buildRows(const SequenceList & sequences, RowList & rows);
+  bool buildSums(const RowList & rows, NumberList & sums);
+
+  void printNames(std::ostream & output, const SequenceList & sequences);
+  void printRows(std::ostream & output, const RowList & rows);
+  void printSums(std::ostream & output, const NumberList & sums);
 }
 
 #endif
