d8eebb7c62848de895762df672b5bbc1636347f6
sp0146001 <sp0146001@gmail.com>
2026-03-13 20:16:22 +0300

    List class + functions of operate on list elements

--- petrov.sasha/S1/list.hpp
+++ petrov.sasha/S1/list.hpp
@@ -107,6 +107,147 @@ namespace petrov
   private:
     const Node< T >* ptr_;
   };
+
+  template< class T>
+  class List {
+  public:
+    using iteraor = LIter< T >;
+    using const_iterator = LCIter< T >;
+    List():
+      head_(nullptr),
+      tail_(nullptr),
+      size_(0)
+    {}
+
+    List(const List< T >& other):
+      head_(nullptr),
+      tail_(nullptr),
+      size_(0)
+    {
+      try {
+        for(const_iterator it = other.cbegin(); it != other.cend(); ++it) {
+          pushBack(*it);
+        }
+      } catch(...) {
+        clear();
+        throw;
+      }
+    }
+    ~List() {
+      clear();
+    }
+    List< T >& operator=(const List< T >& other) {
+      if (this != std::addressof(other)) {
+        List< T > temp(other);
+        swap(temp);
+      }
+      return *this;
+    }
+    iterator begin() {
+      return iterator(head_);
+    }
+    iterator end() {
+      return iterator(nullptr);
+    }
+    const_iterator begin() const {
+      return cbegin();
+    }
+    const_iterator end() const {
+      return cend();
+    }
+    const_iterator cbegin() const {
+      return const_iterator(head_);
+    }
+    const_iterator cend() const {
+      return const_iterator(nullptr);
+    }
+    T& front() {
+      return head_->value;
+    }
+    const T& front() const {
+      return head_->value;
+    }
+    T& back() {
+      return tail_->value_;
+    }
+    const T& back() const {
+      return tail_->value_;
+    }
+    void pushFront(const T& value) {
+      Node< T >* new_node = new Node< T >(value);
+      new_node->next_ = head_;
+      if (head_ != nullptr) {
+        head_->prev_ = new_node;
+      } else {
+        tail_ = new_node;
+      }
+      head_ = new_node;
+      ++size_;
+    }
+    void pushBack(const T& value) {
+      Node< T >* new_node = new Node< T >(value);
+      new_node->prev_ = tail_;
+      if (tail_ != nullptr) {
+        tail_->next_ = new_node;
+      } else {
+        head_ = new_node;
+      }
+      tail_ = new_node;
+      ++size_;
+    }
+    void popFront() {
+      if (head_ == nullptr) {
+        return;
+      }
+      Node< T >* temp = head_;
+      head_ = head_->next_;
+      if (head_ != nullptr) {
+        head_->prev_ = nullptr;
+      } else {
+        tail_ = nullptr;
+      }
+      delete temp;
+      --size;
+    }
+    void popBack() {
+      if (tail_ == nullptr) {
+        return;
+      }
+      Node< T >* temp = tail_;
+      tail_ = tail_->prev_;
+      if (tail_ != nullptr) {
+        tail_->next_ = nullptr;
+      } else {
+        head_ = nullptr;
+      }
+      delete temp;
+      --size;
+    }
+    void clear() {
+      while (head_ != nullptr) {
+        Node< T >* temp = head_;
+        head = head_->next_;
+        delete temp;
+      }
+      tail_ = nullptr;
+      size_ = 0;
+    }
+    std::size_t size()const  {
+      return size_;
+    }
+    bool empty() const {
+      return size_ == 0;
+    }
+    void swap(List< T >& other) noexcept {
+      std::swap(head_, other.head_);
+      std::swap(tail_, other.tail_);
+      std::swap(size_, other.size_);
+    }
+  private:
+    Node< T >* head_;
+    Node< T >* tail_;
+    std::size_t size_;
+  };
 }
 
 #endif
