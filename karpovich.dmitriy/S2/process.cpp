1998976fbcc70bc97146737ff031f5a4c0e51e6d
voyadger01 <voyadgerkx@gmail.com>
2026-03-14 10:50:37 +0300

    add input and output

--- karpovich.dmitriy/S2/process.cpp
+++ karpovich.dmitriy/S2/process.cpp
@@ -1,6 +1,37 @@
 #include "process.hpp"
 #include "queue.hpp"
+#include "stack.hpp"
+#include <cstddef>
 #include <string>
 
 void karpovich::inputQueue(std::istream &in, Queue< Queue< std::string > > &queue)
-{}
+{
+  std::string s;
+  while (std::getline(in, s)) {
+    if (s.empty()) {
+      continue;
+    }
+    Queue< std::string > inf;
+    for (size_t i = 0; i < s.length(); i++) {
+      std::string sChar = s.substr(i, 1);
+      if (sChar == " ") {
+        continue;
+      }
+      inf.push(sChar);
+    }
+    queue.push(inf);
+  }
+}
+void karpovich::output(std::ostream &out, Stack< long long > res)
+{
+  if (!res.empty()) {
+    out << res.top();
+    res.pop();
+  }
+  while (!res.empty()) {
+    out << ' ';
+    out << res.top();
+    res.pop();
+  }
+  out << '\n';
+}
