425e96eeb3caec57cde4ea6b0ed41b38219b46f7
Kayv1e <krutojpacanivan@gmail.com>
2026-03-14 18:21:01 +0300

    Implemented a few methods

--- novikov.ivan/S1/list.hpp
+++ novikov.ivan/S1/list.hpp
@@ -10,25 +10,27 @@ namespace novikov
   class LIter
   {
   public:
-    bool operator==(const LIter< T >& other);
-    bool operator!=(LIter< T > other);
-    LIter< T > operator++();
-    LIter< T > operator--();
+    explicit LIter(typename List< T >::Node*);
+    bool operator==(const LIter< T >&) const noexcept;
+    bool operator!=(const LIter< T >&) const noexcept;
+    LIter< T >& operator++();
+    LIter< T >& operator--();
     T& operator*();
 
   private:
     friend class List< T >;
-    const typename List< T >::Node* curr = nullptr;
+    typename List< T >::Node* curr = nullptr;
   };
 
   template < class T >
   class LCIter
   {
   public:
-    bool operator==(LCIter< T > other);
-    bool operator!=(LCIter< T > other);
-    LCIter< T > operator++();
-    LCIter< T > operator--();
+    explicit LCIter(const typename List< T >::Node*);
+    bool operator==(const LCIter< T >&);
+    bool operator!=(const LCIter< T >&);
+    LCIter< T >& operator++();
+    LCIter< T >& operator--();
     const T& operator*();
 
   private:
@@ -46,23 +48,21 @@ namespace novikov
       Node* next = nullptr;
       Node* prev = nullptr;
     };
-    List();
+    List() = default;
 
     List(const List< T >&);
     List(List< T >&&);
 
-    ~List() noexcept;
+    ~List();
 
-    List< T > operator=(const List< T >&);
-    List< T > operator=(List< T >&&);
+    List< T >& operator=(const List< T >&);
+    List< T >& operator=(List< T >&&);
 
     LIter< T > begin();
-    LCIter< T > begin() const;
-    LCIter< T > cbegin() const noexcept;
+    LCIter< T > cbegin() const;
 
     LIter< T > end();
-    LCIter< T > end() const;
-    LCIter< T > cend() const noexcept;
+    LCIter< T > cend() const;
 
     T& front();
     const T& front() const;
@@ -77,13 +77,73 @@ namespace novikov
     void pop_front();
     void pop_back();
 
-    void clear();
-    bool empty() const;
+    void clear() noexcept;
+    bool empty() const noexcept;
 
   private:
     Node* head = nullptr;
     Node* tail = nullptr;
   };
+}
+
+template < class T >
+bool novikov::List< T >::empty() const noexcept
+{
+  return !head;
+}
+
+template < class T >
+novikov::List< T >::~List()
+{
+  clear();
+}
+
+template < class T >
+void novikov::List< T >::clear() noexcept
+{
+  while (head)
+  {
+    Node* next = head->next;
+    delete head;
+    head = next;
+  }
+  head = nullptr;
+  tail = nullptr;
+}
+
+template < class T >
+novikov::LIter< T > novikov::List< T >::begin()
+{
+  return novikov::LIter< T >(head);
+}
+
+template < class T >
+novikov::LIter< T > novikov::List< T >::end()
+{
+  return novikov::LIter< T >(nullptr);
+}
+
+template < class T >
+novikov::LCIter< T > novikov::List< T >::cbegin() const
+{
+  return novikov::LCIter< T >(head);
+}
+
+template < class T >
+novikov::LCIter< T > novikov::List< T >::cend() const
+{
+  return novikov::LCIter< T >(nullptr);
+}
+
+template < class T >
+bool novikov::LIter< T >::operator==(const novikov::LIter< T >& other) const noexcept
+{
+  return curr == other.curr;
+}
+
+template < class T >
+novikov::LIter< T >::LIter(typename List< T >::Node* node): curr(node)
+{
 }
 
 #endif
