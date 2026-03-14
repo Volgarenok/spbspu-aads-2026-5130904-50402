0afafdf518d1fa89cbb7c76159ea4079f20982e4
R0me0Star <eip_spb@mail.ru>
2026-03-14 16:47:24 +0300

    better tests

--- pozdnyakov.egor/S1/test.cpp
+++ pozdnyakov.egor/S1/test.cpp
@@ -1,11 +1,50 @@
-#include <boost/test/unit_test.hpp>
-#include "process.hpp"
+#define BOOST_TEST_MODULE S1_TESTS
+#include <boost/test/included/unit_test.hpp>
 #include "list.hpp"
-#include <vector>
+#include "process.hpp"
+#include <initializer_list>
+#include <stdexcept>
 
-BOOST_AUTO_TEST_SUITE(ProcessTests)
+BOOST_AUTO_TEST_SUITE(ListTests)
 
-pozdnyakov::List< pozdnyakov::ValueType > makeList(const std::vector< pozdnyakov::ValueType > &vals)
+BOOST_AUTO_TEST_CASE(testListInsertAndIterators)
+{
+  pozdnyakov::List< int > list;
+  BOOST_TEST(list.empty());
+
+  list.pushFront(10);
+  BOOST_TEST(list.front() == 10);
+
+  list.insertAfter(list.begin(), 20);
+
+  int expected[] = {10, 20};
+  int i = 0;
+  for (auto it = list.begin(); it != list.end(); ++it) {
+    BOOST_TEST(*it == expected[i]);
+    ++i;
+  }
+}
+
+BOOST_AUTO_TEST_CASE(testListCopyExceptionSafety)
+{
+  pozdnyakov::List< int > list1;
+  list1.pushFront(2);
+  list1.pushFront(1);
+
+  pozdnyakov::List< int > list2 = list1;
+  BOOST_TEST(!list2.empty());
+  BOOST_TEST(list2.front() == 1);
+
+  list2.insertAfter(list2.begin(), 3);
+
+  auto it = list1.begin();
+  ++it;
+  BOOST_TEST(*it == 2);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+pozdnyakov::List< pozdnyakov::ValueType > makeList(std::initializer_list< pozdnyakov::ValueType > vals)
 {
   pozdnyakov::List< pozdnyakov::ValueType > lst;
   pozdnyakov::LIter< pozdnyakov::ValueType > tail = lst.end();
@@ -21,21 +60,43 @@ pozdnyakov::List< pozdnyakov::ValueType > makeList(const std::vector< pozdnyakov
   return lst;
 }
 
-BOOST_AUTO_TEST_CASE(testProcessLogicCorrectness)
+BOOST_AUTO_TEST_SUITE(ProcessTests)
+
+BOOST_AUTO_TEST_CASE(testEmptySequences)
 {
   pozdnyakov::List< pozdnyakov::NamedSequence > seqs;
-  seqs.pushFront({"fourth", makeList({4, 4})});
-  seqs.pushFront({"third", makeList({})});
-  seqs.pushFront({"second", makeList({2, 2, 2, 2})});
-  seqs.pushFront({"first", makeList({1, 1, 1})});
 
   pozdnyakov::ProcessResult res = pozdnyakov::buildInterleavedRows(seqs);
 
-  std::vector< pozdnyakov::ValueType > expectedSums = {7, 7, 3, 2};
+  BOOST_TEST(res.rows.empty());
+  BOOST_TEST(res.sums.empty());
+}
+
+BOOST_AUTO_TEST_CASE(testSequencesWithoutNumbers)
+{
+  pozdnyakov::List< pozdnyakov::NamedSequence > seqs;
+  seqs.pushFront({"second", makeList({})});
+  seqs.pushFront({"first", makeList({})});
+
+  pozdnyakov::ProcessResult res = pozdnyakov::buildInterleavedRows(seqs);
+
+  BOOST_TEST(res.rows.empty());
+  BOOST_TEST(res.sums.empty());
+}
+
+BOOST_AUTO_TEST_CASE(testSingleSequence)
+{
+  pozdnyakov::List< pozdnyakov::NamedSequence > seqs;
+  seqs.pushFront({"single", makeList({10, 20})});
+
+  pozdnyakov::ProcessResult res = pozdnyakov::buildInterleavedRows(seqs);
+
+  pozdnyakov::ValueType expectedSums[] = {10, 20};
   int i = 0;
   for (auto it = res.sums.begin(); it != res.sums.end(); ++it) {
     BOOST_TEST(*it == expectedSums[i++]);
   }
+  BOOST_TEST(i == 2);
 }
 
 BOOST_AUTO_TEST_CASE(testProcessLogicOverflow)
