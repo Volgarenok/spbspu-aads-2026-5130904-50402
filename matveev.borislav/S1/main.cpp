57b45ac38770472124c79ddd20e7999a970ecdc2
L1sa620 <lisevbom@gmail.com>
2026-03-15 18:47:05 +0300

    Fix CG 6

--- matveev.borislav/S1/main.cpp
+++ matveev.borislav/S1/main.cpp
@@ -1,87 +1,98 @@
 #include <iostream>
 #include <string>
 #include <utility>
-#include <limits>
 #include <climits>
-#include <cctype>
+#include <limits>
 
 #include "list.hpp"
 
 int main()
 {
-  matveev::List< std::pair< std::string, matveev::List< unsigned long long > > > sequences;
+  matveev::List<std::pair<std::string, matveev::List<long long>>> sequences;
   auto tail = sequences.beforeBegin();
   std::string name;
 
   while (std::cin >> name)
   {
-    matveev::List< unsigned long long > numbers;
+    matveev::List<long long> numbers;
     auto numTail = numbers.beforeBegin();
-    unsigned long long value;
+    long long value;
 
-    while (std::cin.peek() != '\n' && std::cin.peek() != EOF)
+    while (true)
     {
-      if (std::isspace(std::cin.peek()))
+      int c = std::cin.peek();
+
+      if (c == '\n' || c == EOF)
       {
-        std::cin.ignore();
-        continue;
+        break;
       }
 
       if (!(std::cin >> value))
       {
-        std::cerr << "Error\n";
-        return 1;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        break;
       }
+
       numTail = numbers.insertAfter(numTail, value);
     }
 
-    std::pair< std::string, matveev::List< unsigned long long > > seq(name, numbers);
+    std::pair<std::string, matveev::List<long long>> seq(name, numbers);
     tail = sequences.insertAfter(tail, seq);
   }
 
   if (sequences.begin() == sequences.end())
   {
+    std::cout << 0 << "\n";
     return 0;
   }
 
-  matveev::List< unsigned long long > sums;
+  auto it = sequences.begin();
+  std::cout << it->first;
+  ++it;
+
+  for (; it != sequences.end(); ++it)
+  {
+    std::cout << " " << it->first;
+  }
+
+  std::cout << "\n";
+
+  matveev::List<long long> sums;
   auto sumTail = sums.beforeBegin();
+
   bool more = true;
-  size_t rowIndex = 0;
 
   while (more)
   {
     more = false;
-    unsigned long long rowSum = 0;
-    bool hasValueInRow = false;
+    long long rowSum = 0;
+    bool first = true;
 
     for (auto it = sequences.begin(); it != sequences.end(); ++it)
     {
       auto &list = it->second;
-      auto valIt = list.begin();
 
-      for (size_t i = 0; i < rowIndex && valIt != list.end(); ++i)
+      if (list.begin() != list.end())
       {
-        ++valIt;
-      }
+        long long v = *list.begin();
 
-      if (valIt != list.end())
-      {
-        unsigned long long v = *valIt;
-        if (hasValueInRow)
+        if (rowSum > LLONG_MAX - v)
         {
-          if (v > (ULLONG_MAX - rowSum))
-          {
-            std::cerr << "Error\n";
-            return 1;
-          }
-          rowSum += v;
+          std::cerr << "Error\n";
+          return 1;
         }
-        else
+
+        if (!first)
         {
-          rowSum = v;
-          hasValueInRow = true;
+          std::cout << " ";
         }
+
+        std::cout << v;
+        first = false;
+
+        rowSum += v;
+        list.removeFront();
         more = true;
       }
     }
@@ -89,56 +100,26 @@ int main()
     if (more)
     {
       sumTail = sums.insertAfter(sumTail, rowSum);
-      rowIndex++;
+      std::cout << "\n";
     }
-  }
-
-  auto it = sequences.begin();
-  std::cout << it->first;
-  for (++it; it != sequences.end(); ++it)
-  {
-    std::cout << " " << it->first;
-  }
-  std::cout << "\n";
-
-  for (size_t i = 0; i < rowIndex; ++i)
-  {
-    bool firstInRow = true;
-    for (auto sit = sequences.begin(); sit != sequences.end(); ++sit)
-    {
-      auto valIt = sit->second.begin();
-      for (size_t j = 0; j < i && valIt != sit->second.end(); ++j)
-      {
-        ++valIt;
-      }
-
-      if (valIt != sit->second.end())
-      {
-        if (!firstInRow)
-        {
-          std::cout << " ";
-        }
-        std::cout << *valIt;
-        firstInRow = false;
-      }
-    }
-    std::cout << "\n";
   }
 
   if (sums.begin() == sums.end())
   {
     std::cout << 0 << "\n";
+    return 0;
   }
-  else
+
+  auto sit = sums.begin();
+  std::cout << *sit;
+  ++sit;
+
+  for (; sit != sums.end(); ++sit)
   {
-    auto sit = sums.begin();
-    std::cout << *sit;
-    for (++sit; sit != sums.end(); ++sit)
-    {
-      std::cout << " " << *sit;
-    }
-    std::cout << "\n";
+    std::cout << " " << *sit;
   }
+
+  std::cout << "\n";
 
   return 0;
 }
