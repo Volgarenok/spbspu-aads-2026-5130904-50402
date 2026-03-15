8a2709cd1f2224ef1c5337fbbca0227aca1428f4
L1sa620 <lisevbom@gmail.com>
2026-03-15 13:44:40 +0300

    Fix implementation input

--- matveev.borislav/S1/main.cpp
+++ matveev.borislav/S1/main.cpp
@@ -5,39 +5,45 @@
 
 int main()
 {
-  matveev::List< std::pair< std::string, matveev::List< int > > > sequences;
+  matveev::List<std::pair<std::string,matveev::List<int>>> sequences;
+  auto tail = sequences.beforeBegin();
   std::string name;
 
-  while (std::cin >> name)
+  while(std::cin>>name)
   {
-    matveev::List< int > numbers;
+    matveev::List<int> numbers;
     int value;
+    auto numTail = numbers.beforeBegin();
 
-    while (true)
+    while(true)
     {
-      if (std::cin.peek() == '\n' || std::cin.peek() == EOF)
+      int c = std::cin.peek();
+
+      if(c=='\n'||c==EOF)
       {
         break;
       }
 
-      if (!(std::cin >> value))
+      if(!(std::cin>>value))
       {
+        std::cin.clear();
         break;
       }
 
-      numbers.insertAfter(numbers.beforeBegin(), value);
+      numTail = numbers.insertAfter(numTail,value);
     }
 
-    std::pair< std::string, matveev::List< int > > seq(name, numbers);
-    sequences.insertAfter(sequences.beforeBegin(), seq);
+    std::pair<std::string,matveev::List<int>> seq(name,numbers);
+    tail = sequences.insertAfter(tail,seq);
   }
 
-  for (matveev::LIter< std::pair< std::string, matveev::List< int > > > it =
-       sequences.begin(); it != sequences.end(); ++it)
+  for(matveev::LIter<std::pair<std::string,matveev::List<int>>> it = sequences.begin();
+      it != sequences.end();
+      ++it)
   {
-    std::cout << it->first << " ";
+    std::cout<<it->first<<" ";
   }
 
-  std::cout << "\n";
+  std::cout<<"\n";
   return 0;
 }
