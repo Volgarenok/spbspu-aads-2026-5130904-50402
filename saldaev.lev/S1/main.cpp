faf04ae6a936b933d8efd4f48eece59cc8023d7c
Carrot_lord <lev.saldaev@gmai.com>
2026-03-16 02:32:02 +0300

    fix overflow error handling

--- saldaev.lev/S1/main.cpp
+++ saldaev.lev/S1/main.cpp
@@ -75,33 +75,37 @@ int main()
   }
 
   saldaev::List< size_t > sums;
-  for (size_t i = 0; i < iters.getLength(); ++i) {
-    sums.newHead(0);
-  }
-  saldaev::LIter< size_t > sumsIt = sums.begin();
-  any_left = true;
-  while (any_left) {
-    any_left = false;
-    saldaev::LIter< saldaev::LIter< size_t > > iterIt = iters.begin();
-
-    while (iterIt != iters.end()) {
-      saldaev::LIter< size_t > &innerIt = iterIt.getData();
-      if (innerIt.isValid()) {
-        size_t num = innerIt.getData();
-        if (std::numeric_limits< size_t >::max() - num < sumsIt.getData()) {
-          throw std::overflow_error("overflow_error");
-        }
-        sumsIt.getData() += innerIt.getData();
-        ++innerIt;
-        any_left = true;
-        first = false;
-      }
-      ++iterIt;
+  try {
+    for (size_t i = 0; i < iters.getLength(); ++i) {
+      sums.newHead(0);
     }
-    ++sumsIt;
+    saldaev::LIter< size_t > sumsIt = sums.begin();
+    any_left = true;
+    while (any_left) {
+      any_left = false;
+      saldaev::LIter< saldaev::LIter< size_t > > iterIt = iters.begin();
+
+      while (iterIt != iters.end()) {
+        saldaev::LIter< size_t > &innerIt = iterIt.getData();
+        if (innerIt.isValid()) {
+          size_t num = innerIt.getData();
+          if (std::numeric_limits< size_t >::max() - num < sumsIt.getData()) {
+            throw std::overflow_error("overflow_error");
+          }
+          sumsIt.getData() += innerIt.getData();
+          ++innerIt;
+          any_left = true;
+          first = false;
+        }
+        ++iterIt;
+      }
+      ++sumsIt;
+    }
+  } catch (...) {
+    return 1;
   }
 
-  sumsIt = sums.begin();
+  saldaev::LIter< size_t > sumsIt = sums.begin();
   first = true;
   while (sumsIt != sums.end()) {
     if (!first) {
