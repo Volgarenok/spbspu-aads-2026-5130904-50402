82612cc37006c3b593a5a60003740752b2cec808
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-03-18 20:17:32 +0300

    add output func and make process func safety

--- lachugin.mikhail/S1/tools.cpp
+++ lachugin.mikhail/S1/tools.cpp
@@ -50,96 +50,133 @@ lachugin::List < lachugin::pair > lachugin::getline(std::istream& in)
   return listOfLists;
 }
 
-lachugin::List< lachugin::List< int > > lachugin::process(List< pair > l) {
+lachugin::List< lachugin::List< int > > lachugin::process(List< pair > l)
+{
   List< List< int > > listOfLists{};
   Node< List< int > >* currOfLists = nullptr;
-
   size_t k = 0;
-
-  while (true)
+  try
   {
-    List< int > listOfVal{};
-    Node< int >* curr = nullptr;
-
-    bool added = false;
-
-    auto iterator = l.begin();
-
-    while (iterator != l.end())
+    while (true)
     {
-      auto it = (*iterator).second.begin();
+      List< int > listOfVal{};
+      Node< int >* curr = nullptr;
+      bool added = false;
+      auto iterator = l.begin();
 
-      size_t i = 0;
-      while (i < k && it != (*iterator).second.end())
+      while (iterator != l.end())
       {
-        ++it;
-        ++i;
+        auto it = (*iterator).second.begin();
+        size_t i = 0;
+        while (i < k && it != (*iterator).second.end())
+        {
+          ++it;
+          ++i;
+        }
+
+        if (it != (*iterator).second.end())
+        {
+          if (curr == nullptr)
+          {
+            curr = listOfVal.add(*it);
+          }
+          else
+          {
+            curr = listOfVal.addNext(*it, curr);
+          }
+          added = true;
+        }
+
+        ++iterator;
       }
 
-      if (it != (*iterator).second.end())
+      if (!added)
       {
-        if (curr == nullptr)
-        {
-          curr = listOfVal.add(*it);
-        }
-        else
-        {
-          curr = listOfVal.addNext(*it, curr);
-        }
-        added = true;
+        break;
+      }
+      if (currOfLists == nullptr)
+      {
+        currOfLists = listOfLists.add(listOfVal);
+      }
+      else
+      {
+        currOfLists = listOfLists.addNext(listOfVal, currOfLists);
+      }
+      ++k;
+    }
+
+    List< int > sumList{};
+    Node< int >* currSum = nullptr;
+    auto itLists = listOfLists.begin();
+
+    while (itLists != listOfLists.end())
+    {
+      int sum = 0;
+      auto itVals = (*itLists).begin();
+      while (itVals != (*itLists).end())
+      {
+        sum += *itVals;
+        ++itVals;
       }
 
-      ++iterator;
+      if (currSum == nullptr)
+      {
+        currSum = sumList.add(sum);
+      }
+      else
+      {
+        currSum = sumList.addNext(sum, currSum);
+      }
+      ++itLists;
     }
 
-    if (!added)
-    {
-      break;
-    }
     if (currOfLists == nullptr)
     {
-      currOfLists = listOfLists.add(listOfVal);
+      listOfLists.add(sumList);
     }
     else
     {
-      currOfLists = listOfLists.addNext(listOfVal, currOfLists);
+      listOfLists.addNext(sumList, currOfLists);
     }
-    ++k;
   }
-
-  List<int> sumList{};
-  Node<int>* currSum = nullptr;
-
-  auto itLists = listOfLists.begin();
-
-  while (itLists != listOfLists.end())
+  catch (const std::bad_alloc& e)
   {
-    int sum = 0;
-    auto itVals = (*itLists).begin();
-    while (itVals != (*itLists).end())
-    {
-      sum += *itVals;
-      ++itVals;
-    }
+    std::cout << e.what();
 
-    if (currSum == nullptr)
+    auto itDelete = listOfLists.begin();
+    while (itDelete != listOfLists.end())
     {
-      currSum = sumList.add(sum);
+      (*itDelete).clear();
+      ++itDelete;
     }
-    else
-    {
-      currSum = sumList.addNext(sum, currSum);
-    }
-    ++itLists;
+    listOfLists.clear();
+    throw;
   }
 
-  if (currOfLists == nullptr)
-  {
-    listOfLists.add(sumList);
-  }
-  else
-  {
-    listOfLists.addNext(sumList, currOfLists);
-  }
   return listOfLists;
 }
+
+void lachugin::output(List< List< int > > lVal, List< pair > l)
+{
+  auto it = l.begin();
+
+  while (it != l.end()) {
+    std::cout << (*it).first << " ";
+    ++it;
+  }
+  std::cout << "\n";
+  auto itVal = lVal.begin();
+
+  while (itVal != lVal.end())
+  {
+    auto iterator = (*itVal).begin();
+    while (iterator != (*itVal).end())
+    {
+      std::cout << (*iterator) << " ";
+      ++iterator;
+    }
+    std::cout << " \n";
+    ++itVal;
+  }
+}
+
--- lachugin.mikhail/S1/tools.hpp
+++ lachugin.mikhail/S1/tools.hpp
@@ -7,5 +7,6 @@ namespace lachugin
   using pair = std::pair< std::string, List< int > >;
   List < pair > getline(std::istream& in);
   List < List < int > > process(List< pair > l);
+  void output(List< List< int > > lVal, List< pair > l);
 }
 #endif
