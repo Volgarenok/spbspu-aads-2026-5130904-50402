cb69758b779136329cd0cf6813e7207fd7ef9693
L1sa620 <lisevbom@gmail.com>
2026-03-20 20:01:44 +0300

    Add size tracking

--- matveev.borislav/S2/stack.hpp
+++ matveev.borislav/S2/stack.hpp
@@ -2,6 +2,8 @@
 #define MATVEEV_STACK_HPP
 
 #include "../common/list.hpp"
+#include <stdexcept>
+#include <cstddef>
 
 namespace matveev
 {
@@ -9,8 +11,57 @@ namespace matveev
 template<class T>
 class Stack
 {
+public:
+
+  Stack():
+    size_(0)
+  {}
+
+  bool empty() const
+  {
+    return size_ == 0;
+  }
+
+  size_t size() const
+  {
+    return size_;
+  }
+
+  void push(const T& value)
+  {
+    data_.insertAfter(data_.beforeBegin(), value);
+    ++size_;
+  }
+
+  T drop()
+  {
+    if (size_ == 0)
+    {
+      throw std::runtime_error("stack empty");
+    }
+
+    auto it = data_.begin();
+    T value = *it;
+
+    data_.removeFront();
+    --size_;
+
+    return value;
+  }
+
+  T& top()
+  {
+    if (size_ == 0)
+    {
+      throw std::runtime_error("stack empty");
+    }
+
+    return *data_.begin();
+  }
+
 private:
   List<T> data_;
+  size_t size_;
 };
 
 }
