b7f010aff825643a43de7f7c59f901540cdd9666
L1sa620 <lisevbom@gmail.com>
2026-03-20 20:20:19 +0300

    Add operators logic

--- matveev.borislav/S2/parser.hpp
+++ matveev.borislav/S2/parser.hpp
@@ -1,6 +1,11 @@
 #ifndef MATVEEV_PARSER_HPP
 #define MATVEEV_PARSER_HPP
 
+#include "stack.hpp"
+#include "queue.hpp"
+#include <string>
+#include <cctype>
+
 namespace matveev
 {
 
@@ -14,6 +19,71 @@ long gcd(long a, long b)
   }
 
   return a;
+}
+
+bool isOperator(const std::string& op)
+{
+  return op == "+"
+      || op == "-"
+      || op == "*"
+      || op == "/"
+      || op == "%"
+      || op == "^";
+}
+
+bool isNumber(const std::string& token)
+{
+  if (token.empty())
+  {
+    return false;
+  }
+
+  size_t i = 0;
+
+  if (token[0] == '-' && token.size() > 1)
+  {
+    i = 1;
+  }
+
+  for (; i < token.size(); ++i)
+  {
+    if (!std::isdigit(token[i]))
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+bool isGcd(const std::string& op)
+{
+  return op == "gcd";
+}
+
+int precedence(const std::string& op)
+{
+  if (op == "+" || op == "-")
+  {
+    return 1;
+  }
+
+  if (op == "*" || op == "/" || op == "%")
+  {
+    return 2;
+  }
+
+  if (op == "^")
+  {
+    return 3;
+  }
+
+  if (op == "gcd")
+  {
+    return 2;
+  }
+
+  return 0;
 }
 
 }
