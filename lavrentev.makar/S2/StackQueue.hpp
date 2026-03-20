c306cb08d2e77976cca6cf288dd764c6ef23390e
lavrentev.makar <makar.lavrentev@yandex.ru>
2026-03-20 19:04:45 +0300

    Added realization Queue methods with tests according to TDD

--- lavrentev.makar/S2/StackQueue.hpp
+++ lavrentev.makar/S2/StackQueue.hpp
@@ -35,6 +35,67 @@ namespace lavrentev{
 }
 
 template <class T>
+void lavrentev::Queue<T>::push(const T& rhs)
+{
+  if (data.empty())
+  {
+    data.insert(data.begin(), rhs);
+    tail = data.begin();
+  } else {
+    LIter<T> next = tail;
+    tail = data.insert(next, rhs);
+  }
+}
+
+template <class T>
+T lavrentev::Queue<T>::drop()
+{
+  T b = data.front();
+  data.popFront();
+  if((*this).empty())
+  {
+    tail = LIter<T>{};
+  }
+  return b;
+}
+
+template <class T>
+const T& lavrentev::Queue<T>::back() const
+{
+  if(!(*this).empty()){
+    return *tail;
+  }
+  throw std::runtime_error("Queue is empty");
+}
+
+template <class T>
+T& lavrentev::Queue<T>::back()
+{
+  if(!(*this).empty()){
+    return *tail;
+  }
+  throw std::runtime_error("Queue is empty");
+}
+
+template <class T>
+bool lavrentev::Queue<T>::empty() const
+{
+  return data.empty();
+}
+
+template <class T>
+T& lavrentev::Queue<T>::front()
+{
+  return data.front();
+}
+
+template <class T>
+const T& lavrentev::Queue<T>::front() const
+{
+  return data.front();
+}
+
+template <class T>
 void lavrentev::Stack<T>::push(const T& rhs)
 {
   data.pushFront(rhs);
--- lavrentev.makar/S2/test-StackQueue.cpp
+++ lavrentev.makar/S2/test-StackQueue.cpp
@@ -1,16 +1,17 @@
+#include <stdexcept>
 #define BOOST_TEST_MODULE StackQueueTest
 #include <boost/test/included/unit_test.hpp>
 #include <sstream>
 #include "StackQueue.hpp"
 
-BOOST_AUTO_TEST_CASE(push_test)
+BOOST_AUTO_TEST_CASE(Stack_push_test)
 {
   lavrentev::Stack<size_t> s{};
   s.push(1);
   BOOST_TEST(s.top() == 1);
 }
 
-BOOST_AUTO_TEST_CASE(drop_test)
+BOOST_AUTO_TEST_CASE(Stack_drop_test)
 {
   lavrentev::Stack<size_t> s{};
   BOOST_CHECK_THROW(s.drop(), std::out_of_range);
@@ -21,7 +22,7 @@ BOOST_AUTO_TEST_CASE(drop_test)
   BOOST_TEST(s.top() == 1);
 }
 
-BOOST_AUTO_TEST_CASE(empty_test)
+BOOST_AUTO_TEST_CASE(Stack_empty_test)
 {
   lavrentev::Stack<size_t> s{};
   BOOST_TEST(s.empty());
@@ -29,10 +30,55 @@ BOOST_AUTO_TEST_CASE(empty_test)
   BOOST_TEST(!s.empty());
 }
 
-BOOST_AUTO_TEST_CASE(top_test)
+BOOST_AUTO_TEST_CASE(Stack_top_test)
 {
   lavrentev::Stack<size_t> s{};
   BOOST_CHECK_THROW(s.top(), std::out_of_range);
   s.push(1);
   BOOST_TEST(s.top() == 1);
+}
+
+BOOST_AUTO_TEST_CASE(Queue_push_test)
+{
+  lavrentev::Queue<size_t> s{};
+  s.push(1);
+  BOOST_TEST(s.front() == 1);
+}
+
+BOOST_AUTO_TEST_CASE(Queue_drop_test)
+{
+  lavrentev::Queue<size_t> s{};
+  BOOST_CHECK_THROW(s.drop(), std::out_of_range);
+
+  s.push(1);
+  s.push(2);
+  BOOST_TEST(s.drop() == 1);
+  BOOST_TEST(s.front() == 2);
+}
+
+BOOST_AUTO_TEST_CASE(Queue_back_test)
+{
+  lavrentev::Queue<size_t> s{};
+  BOOST_CHECK_THROW(s.back(), std::runtime_error);
+
+  s.push(1);
+  s.push(2);
+  BOOST_TEST(s.back() == 2);
+}
+
+BOOST_AUTO_TEST_CASE(Queue_empty_test)
+{
+  lavrentev::Queue<size_t> s{};
+  BOOST_TEST(s.empty());
+  s.push(1);
+  BOOST_TEST(!s.empty());
+}
+
+BOOST_AUTO_TEST_CASE(Queue_front_test)
+{
+  lavrentev::Queue<size_t> s{};
+  BOOST_CHECK_THROW(s.front(), std::out_of_range);
+  s.push(1);
+  s.push(2);
+  BOOST_TEST(s.front() == 1);
 }
