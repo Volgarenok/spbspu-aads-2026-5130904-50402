4fa7a6b036c6df6cc92e3d39b4bf64a62dfee017
Alastor20 <nik_d.007@mail.ru>
2026-03-22 21:41:12 +0300

    add convertion func

--- dirko.nikita/S2/calc.cpp
+++ dirko.nikita/S2/calc.cpp
@@ -1,6 +1,9 @@
 #include "calc.hpp"
+#include "queue.hpp"
+#include "stack.hpp"
+#include <cstddef>
 
-bool dirko::priority(const std::string &op1, const std::string &op2)
+bool dirko::getPriority(const std::string &op1, const std::string &op2)
 {
   short pr1 = 0, pr2 = 0;
   if (op1 == "lcm") {
@@ -18,4 +21,51 @@ bool dirko::priority(const std::string &op1, const std::string &op2)
     pr2 = 2;
   }
   return pr1 >= pr2;
+}
+
+bool dirko::isOpt(std::string candidate)
+{
+  bool ret = false;
+  for (size_t i = 0; i < dirko::opt_size; ++i) {
+    ret = ret || (candidate == opts[i]);
+  }
+  return ret;
+}
+
+dirko::Queue< std::string > dirko::convert(dirko::Queue< std::string > inf)
+{
+  Queue< std::string > ret;
+  Stack< std::string > st;
+  while (!inf.empty()) {
+    std::string curr = inf.get();
+    inf.pop();
+    if (curr == "(") {
+      st.push(curr);
+      continue;
+    }
+    if (isOpt(curr)) {
+      while (!st.empty() && getPriority(curr, st.get()) && st.get() != "(") {
+        ret.push(st.get());
+        st.pop();
+      }
+      st.push(curr);
+      continue;
+    }
+    if (curr == ")") {
+      while (!st.empty() && st.get() != "(") {
+        ret.push(st.get());
+        st.pop();
+      }
+      if (!st.empty()) {
+        st.pop();
+      }
+      continue;
+    }
+    ret.push(curr);
+  }
+  while (!st.empty()) {
+    ret.push(st.get());
+    st.pop();
+  }
+  return ret;
 }
--- dirko.nikita/S2/calc.hpp
+++ dirko.nikita/S2/calc.hpp
@@ -4,8 +4,11 @@
 #include "queue.hpp"
 namespace dirko
 {
+  const std::string opts[]{"+", "-", "*", "/", "%", "lcm"};
+  constexpr size_t opt_size = 6;
   Queue< std::string > convert(Queue< std::string > inf);
   long long calc(Queue< std::string > expretion);
-  bool priority(const std::string &op1, const std::string &op2);
+  bool getPriority(const std::string &op1, const std::string &op2);
+  bool isOpt(std::string candidate);
 }
 #endif
