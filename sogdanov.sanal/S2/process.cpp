f5c55d8d398c2b06c44f273a4a879d71d2260b30
kuzRK <sanalsogdanov11188@gmail.com>
2026-03-22 19:34:11 +0300

    input, output and main

---
+++ sogdanov.sanal/S2/process.cpp
@@ -0,0 +1,24 @@
+#include "process.hpp"
+#include <string>
+#include "operators.hpp"
+void sogdanov::input(std::istream& in, Stack<long long>& res) {
+  std::string line;
+  while (std::getline(in, line) {
+    if (line.empty()) {
+      continue;
+    }
+    res.push(sogdanov::evaluate(line));
+  }
+}
+void sogdanov::output(std::istream& out, Stack<long long>& res) {
+  bool first = true;
+  while (!res.empty()) {
+    if (!first) {
+      out << ' ';
+    }
+    out << res.top();
+    res.pop();
+    first = false;
+  }
+  out << '\n';
+}
