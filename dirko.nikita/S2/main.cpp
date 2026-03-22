b816078b2d126c6fd351db21a4c14138610bb561
Alastor20 <nik_d.007@mail.ru>
2026-03-22 22:22:44 +0300

    almost done

--- dirko.nikita/S2/main.cpp
+++ dirko.nikita/S2/main.cpp
@@ -1,3 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include "actions.hpp"
+#include "calc.hpp"
+#include "queue.hpp"
+#include "stack.hpp"
 
-int main()
-{}
+int main(int argc, char *argv[])
+{
+  if (argc > 2) {
+    std::cerr << "too many args\n";
+    return 1;
+  }
+  std::ifstream file;
+  std::istream *is = &std::cin;
+  if (argc == 2) {
+    file.open(argv[1]);
+    if (!file.is_open()) {
+      std::cerr << "failed to open file\n";
+      return 1;
+    }
+    is = &file;
+  }
+  dirko::Queue< long long > res;
+  try {
+    dirko::Queue< dirko::Queue< std::string > > expretions = dirko::input(*is);
+    dirko::Stack< long long > res;
+    while (!expretions.empty()) {
+      dirko::Queue< std::string > post = dirko::convert(expretions.get());
+      expretions.pop();
+      res.push(dirko::calcExpr(post));
+    }
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
+  if (!res.empty()) {
+    std::cout << res.get();
+    res.pop();
+  }
+  while (!res.empty()) {
+    std::cout << ' ' << res.get();
+    res.pop();
+  }
+  std::cout << '\n';
+}
