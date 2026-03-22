4895afcd4b7e30103bcd0fd3f803227524495d58
kuzRK <sanalsogdanov11188@gmail.com>
2026-03-22 14:53:40 +0300

    stack

---
+++ sogdanov.sanal/S2/stack.hpp
@@ -0,0 +1,47 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+#include "../common/list.hpp"
+#include <cstddef>
+#include <stdexcept>
+namespace sogdanov {
+  template<class T>
+  class Stack {
+    List<T> data_;
+  public:
+    void push(const T& v);
+    void pop();
+    bool empty() const noexcept;
+    size_t size() const noexcept;
+    T& top() noexcept;
+    T drop();
+  };
+  template<class T>
+  void push(const T& v) {
+    data_.push_front(v);
+  }
+  template<class T>
+  void pop() {
+    if (data_.empty()) {
+      throw std::runtime_error("pop on empty stack");
+    }
+    data_.pop_front();
+  }
+  template<class T>
+  bool empty() const noexcept {
+    return data_.empty();
+  }
+  template<class T>
+  size_t size() const noexcept {
+    return data_.size();
+  }
+  template<class T>
+  T& top() noexcept {
+    if (data_.empty()) {
+      throw std::runtime_error("top on empty stack");
+    }
+    return *data_.begin();
+  }
+}
+#endif
+
+
