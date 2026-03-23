24b4b4817d9a8ba3ff79f1109ab81dba8b266488
Carrot_lord <lev.saldaev@gmai.com>
2026-03-23 09:29:15 +0300

    Add tests for copy operations (default implementation suffices)

--- saldaev.lev/S2/test-queue.cpp
+++ saldaev.lev/S2/test-queue.cpp
@@ -62,6 +62,54 @@ namespace saldaev
     BOOST_TEST(q.front() == 10);
   }
 
+  BOOST_AUTO_TEST_CASE(copy_constructor)
+  {
+    Queue< int > original;
+    original.push(5);
+    original.push(6);
+
+    Queue< int > copy(original);
+    BOOST_TEST(copy.front() == 5);
+    copy.pop();
+    BOOST_TEST(copy.front() == 6);
+    copy.pop();
+    BOOST_TEST(copy.empty());
+
+    BOOST_TEST(!original.empty());
+    BOOST_TEST(original.front() == 5);
+
+    Queue< int > empty;
+    Queue< int > copyEmpty(empty);
+    BOOST_TEST(copyEmpty.empty());
+  }
+
+  BOOST_AUTO_TEST_CASE(copy_assignment)
+  {
+    Queue< int > a;
+    a.push(10);
+    a.push(20);
+
+    Queue< int > b;
+    b.push(30);
+
+    b = a;
+    BOOST_TEST(b.front() == 10);
+    b.pop();
+    BOOST_TEST(b.front() == 20);
+    b.pop();
+    BOOST_TEST(b.empty());
+
+    BOOST_TEST(a.front() == 10);
+    BOOST_TEST(!a.empty());
+
+    b = b;
+    BOOST_TEST(b.empty());
+
+    Queue< int > empty;
+    b = empty;
+    BOOST_TEST(b.empty());
+  }
+
   BOOST_AUTO_TEST_SUITE_END()
 
 }
