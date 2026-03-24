226e6d6be29fbe1086acc21bd213ea87b8af6e9a
jar <Jarafan3@gmail.com>
2026-03-24 19:22:24 +0300

    Add list hpp

--- afanasev.yaroslav/S2/queue.hpp
+++ afanasev.yaroslav/S2/queue.hpp
@@ -1,4 +1,15 @@
 #ifndef QUEUE_HPP
 #define QUEUE_HPP
 
+namespace afanasev
+{
+  template< typename T >
+  class Queue
+  {
+   public:
+    void push(T rhs);
+    T drop();
+  };
+}
+
 #endif
--- afanasev.yaroslav/S2/stack.hpp
+++ afanasev.yaroslav/S2/stack.hpp
@@ -1,4 +1,21 @@
 #ifndef STACK_HPP
 #define STACK_HPP
 
+namespace afanasev
+{
+  template< typename T >
+	class Stack
+	{
+	 public:
+	  void push(const T &rhs);
+    T &get() noexcept;
+    void pop() noexcept;
+    bool empty() const noexcept;
+    size_t size() const noexcept;
+
+		void push(T rhs);
+		T drop();
+  };
+}
+
 #endif
