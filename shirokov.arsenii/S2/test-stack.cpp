998673dfeb42e90884e039dfff5c97eeb72d2c95
s1nhronn <silekatik@yandex.ru>
2026-03-25 20:21:58 +0300

    compile test for copy push of stack

--- shirokov.arsenii/S2/test-stack.cpp
+++ shirokov.arsenii/S2/test-stack.cpp
@@ -3,6 +3,14 @@
 
 BOOST_AUTO_TEST_CASE(new_stack_is_empty)
 {
-  shirokov::Stack< int > q;
-  BOOST_TEST(q.empty());
+  shirokov::Stack< int > s;
+  BOOST_TEST(s.empty());
+}
+
+BOOST_AUTO_TEST_CASE(copy_push_make_stack_non_empty)
+{
+  shirokov::Stack< int > s;
+  int a = 1;
+  s.push(a);
+  BOOST_TEST(!s.empty());
 }
