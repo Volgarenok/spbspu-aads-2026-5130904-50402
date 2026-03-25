de64e3f1dec059cd67211beee9f6ebdac3e569fe
s1nhronn <silekatik@yandex.ru>
2026-03-25 20:03:32 +0300

    compile tests for pop of queue

--- shirokov.arsenii/S2/test-queue.cpp
+++ shirokov.arsenii/S2/test-queue.cpp
@@ -1,3 +1,5 @@
+#include <stdexcept>
+#include <boost/test/tools/old/interface.hpp>
 #include <boost/test/unit_test.hpp>
 #include "queue.hpp"
 
@@ -30,4 +32,21 @@ BOOST_AUTO_TEST_CASE(front_method_return_first_elem)
   q.push(3);
   const shirokov::Queue< int > rq = q;
   BOOST_TEST(rq.front() == 1);
+}
+
+BOOST_AUTO_TEST_CASE(pop_throws_exception_if_container_is_empty)
+{
+  shirokov::Queue< int > q;
+  BOOST_REQUIRE(q.empty());
+  BOOST_CHECK_THROW(q.pop(), std::out_of_range);
+}
+
+BOOST_AUTO_TEST_CASE(when_using_pop_front_returns_following_element)
+{
+  shirokov::Queue< int > q;
+  q.push(1);
+  q.push(2);
+  q.push(3);
+  q.pop();
+  BOOST_TEST(q.front() == 2);
 }
