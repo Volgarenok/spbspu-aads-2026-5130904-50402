c0f734b3c9c4fe56df7ef4bc41c422427beebf0c
s1nhronn <silekatik@yandex.ru>
2026-03-25 20:30:07 +0300

    compile test for pop of stack

--- shirokov.arsenii/S2/test-stack.cpp
+++ shirokov.arsenii/S2/test-stack.cpp
@@ -31,3 +31,20 @@ BOOST_AUTO_TEST_CASE(top_method_return_last_elem)
   const shirokov::Stack< int > rs = s;
   BOOST_TEST(rs.top() == 3);
 }
+
+BOOST_AUTO_TEST_CASE(pop_throws_exception_if_container_is_empty)
+{
+  shirokov::Stack< int > s;
+  BOOST_REQUIRE(s.empty());
+  BOOST_CHECK_THROW(s.pop(), std::out_of_range);
+}
+
+BOOST_AUTO_TEST_CASE(when_using_pop_front_returns_following_element)
+{
+  shirokov::Stack< int > s;
+  s.push(1);
+  s.push(2);
+  s.push(3);
+  s.pop();
+  BOOST_TEST(s.top() == 2);
+}
