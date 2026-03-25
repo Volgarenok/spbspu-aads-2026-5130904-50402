817d1b113ea1f616b8fc7ff2c26bf75a2e0c1d77
DalveShutnik <119686844+DalveShutnik@users.noreply.github.com>
2026-03-25 20:47:34 +0300

    add class LIter

--- samarin.denis/s1/main.cpp
+++ samarin.denis/s1/main.cpp
@@ -6,6 +6,31 @@ int main() {
 
 }
 
+template< class T > class List;
+
+template< class T >
+class LIter {
+  friend class List< T >;
+  Node< T > * node;
+public:
+  explicit LIter(Node< T > * n): node(n) {}
+  T& operator*() {
+    return node->value;
+  }
+  T* operator->() {
+    return &node->value;
+  }
+  LIter< T > & operator++() {
+    node = node->next;
+    return *this;
+  }
+  LIter< T > operator++(int) {
+    Liter< T > tmp = *this;
+    node = node->next;
+    return tmp;
+  }
+};
+
 template<class T>
 class List {
   struct Node { T value; Node * next; };
