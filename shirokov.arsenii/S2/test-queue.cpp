69c53d5c38b6b5f55f0b858ee2d4c7805d643abd
s1nhronn <silekatik@yandex.ru>
2026-03-25 19:29:18 +0300

    compile test for copy push of queue

--- shirokov.arsenii/S2/test-queue.cpp
+++ shirokov.arsenii/S2/test-queue.cpp
@@ -6,3 +6,11 @@ BOOST_AUTO_TEST_CASE(new_queue_is_empty)
   shirokov::Queue< int > q;
   BOOST_TEST(q.empty());
 }
+
+BOOST_AUTO_TEST_CASE(copy_push_make_queue_non_empty)
+{
+  shirokov::Queue< int > q;
+  int a = 1;
+  q.push(a);
+  BOOST_TEST(!q.empty());
+}
