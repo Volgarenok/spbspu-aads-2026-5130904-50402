17d84ab0fb99a591975d76570b384406f31b979f
DalveShutnik <119686844+DalveShutnik@users.noreply.github.com>
2026-03-25 20:46:37 +0300

    add class List

--- samarin.denis/s1/main.cpp
+++ samarin.denis/s1/main.cpp
@@ -6,5 +6,15 @@ int main() {
 
 }
 
-
+template<class T>
+class List {
+  struct Node { T value; Node * next; };
+  Node* head;
+public:
+  T value;
+  List< T >* next;
+  void push(const T& v);
+  void clear();
+  LIter<T> begin() { return LIter< T >(head->next); }
+};
 
