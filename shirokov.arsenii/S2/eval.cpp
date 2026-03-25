2926160aaacf6b609646845b99072bd5f8d926c9
s1nhronn <silekatik@yandex.ru>
2026-03-25 12:55:30 +0300

    getPriority, isOperation and infixToPostfix funcs

--- shirokov.arsenii/S2/eval.cpp
+++ shirokov.arsenii/S2/eval.cpp
@@ -1,18 +1,100 @@
 #include "eval.hpp"
+#include <string>
+#include "queue.hpp"
 #include "stack.hpp"
+
+namespace
+{
+  int getPriority(const std::string& op)
+  {
+    if (op == "<<")
+    {
+      return 3;
+    }
+    if (op == "*" || op == "/" || op == "%")
+    {
+      return 2;
+    }
+    if (op == "+" || op == "-")
+    {
+      return 1;
+    }
+    return 0;
+  }
+
+  bool isOperation(const std::string& s)
+  {
+    bool res = s == "<<";
+    res = res || s == "*" || s == "/" || s == "%";
+    res = res || s == "+" || s == "-";
+    return res;
+  }
+
+  shirokov::Queue< std::string > infixToPostfix(shirokov::Queue< std::string > infix)
+  {
+    shirokov::Queue< std::string > postfix;
+    shirokov::Stack< std::string > stack;
+    while (!postfix.empty())
+    {
+      std::string val = infix.front();
+      if (val == "(")
+      {
+        stack.push(val);
+      }
+      else if (val == ")")
+      {
+        std::string op = stack.top();
+        while (isOperation(op))
+        {
+          postfix.push(op);
+          stack.pop();
+          op = stack.top();
+        }
+        if (stack.top() == "(")
+        {
+          stack.pop();
+        }
+      }
+      else if (isOperation(val))
+      {
+        std::string op = stack.top();
+        while (getPriority(op) <= getPriority(val))
+        {
+          postfix.push(op);
+          stack.pop();
+          op = stack.top();
+        }
+        stack.push(val);
+      }
+      else
+      {
+        postfix.push(val);
+      }
+      infix.pop();
+    }
+    while (!stack.empty())
+    {
+      postfix.push(stack.top());
+      stack.pop();
+    }
+    return postfix;
+  }
+
+  shirokov::Queue< std::string > split(const std::string& line);
+}
 
 long long shirokov::eval(std::string line)
 {
-  std::string postfix = infixToPostfix(line);
-  Queue< std::string > exp = split(line);
+  Queue< std::string > infix = split(line);
+  Queue< std::string > postfix = infixToPostfix(infix);
   Stack< long long > results;
   results.push(0);
   long long operand1 = 0, operand2 = 0;
   int status = 0;
 
-  while (!exp.empty())
+  while (!postfix.empty())
   {
-    std::string val = exp.front();
+    std::string val = postfix.front();
     if (val == "+")
     {
       if (status == 2)
@@ -146,7 +228,7 @@ long long shirokov::eval(std::string line)
         status = 2;
       }
     }
-    exp.pop();
+    postfix.pop();
   }
   return results.top();
 }
--- shirokov.arsenii/S2/eval.hpp
+++ shirokov.arsenii/S2/eval.hpp
@@ -1,13 +1,6 @@
 #ifndef EVAL_HPP
 #define EVAL_HPP
 #include <string>
-#include "queue.hpp"
-
-namespace
-{
-  std::string infixToPostfix(const std::string& infix);
-  shirokov::Queue< std::string > split(const std::string& line);
-}
 
 namespace shirokov
 {
