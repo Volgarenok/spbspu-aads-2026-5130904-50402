fac7d22fb98ba9eabc91f547b84cc3c3d728c219
lavrentev.makar <makar.lavrentev@yandex.ru>
2026-03-25 19:08:37 +0300

    Optimise main.cpp

--- lavrentev.makar/S2/main.cpp
+++ lavrentev.makar/S2/main.cpp
@@ -5,61 +5,42 @@
 int main(int argc, char *argv[])
 {
   std::string name;
+  lavrentev::Stack<long long> res;
   try
   {
     if (argc == 2)
     {
-      lavrentev::Stack<long long> res = lavrentev::readFile(argv[1]);
-      if (res.empty())
-      {
-        std::cout << "\n";
-        return 0;
-      }
-      bool first = true;
-      while (!res.empty())
-      {
-        if (!first)
-        {
-          std::cout << " " << res.top();
-        }
-        else
-        {
-          std::cout << res.top();
-          first = false;
-        }
-        res.drop();
-      }
-      std::cout << "\n";
+      res = lavrentev::readFile(argv[1]);
     }
     else if (argc == 1)
     {
-      lavrentev::Stack<long long> res = lavrentev::getline();
-      if (res.empty())
-      {
-        std::cout << "\n";
-        return 0;
-      }
-      bool first = true;
-      while (!res.empty())
-      {
-        if (!first)
-        {
-          std::cout << " " << res.top();
-        }
-        else
-        {
-          std::cout << res.top();
-          first = false;
-        }
-        res.drop();
-      }
-      std::cout << "\n";
+      res = lavrentev::getline();
     }
     else
     {
       std::cout << "\n";
       return 0;
     }
+    if (res.empty())
+      {
+        std::cout << "\n";
+        return 0;
+      }
+      bool first = true;
+      while (!res.empty())
+      {
+        if (!first)
+        {
+          std::cout << " " << res.top();
+        }
+        else
+        {
+          std::cout << res.top();
+          first = false;
+        }
+        res.drop();
+      }
+      std::cout << "\n";
   } catch (const std::exception &e)
   {
     std::cerr << "Input processing error" << "\n";
