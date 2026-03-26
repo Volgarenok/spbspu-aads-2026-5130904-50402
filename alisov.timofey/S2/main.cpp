6e5f7fc7f3a8f586976d84d8a7c450db0cfab678
AlisovTimofey <timofeyalisov@gmail.com>
2026-03-26 22:37:58 +0300

    add Queue and Stack

--- alisov.timofey/S2/main.cpp
+++ alisov.timofey/S2/main.cpp
@@ -1,3 +1,24 @@
+namespace alisov
+{
+  template < typename T > class Queue
+  {
+  public:
+    void push(T rhs);
+    T drop();
+
+  private:
+    list< T > data_;
+  };
+  template < typename T > class Stack
+  {
+  public:
+    void push(T rhs);
+    T drop();
+
+  private:
+    list< T > data_;
+  };
+}
 int main()
 {
 }
