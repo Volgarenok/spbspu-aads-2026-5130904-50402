2417819369c7f29c859dd5e2b30eca0323d271a0
hihikomori <ghhhhhnika@gmail.com>
2026-03-26 19:04:23 +0300

    TDD wrong methods

--- strelnikov.matvey/S2/datastructs.hpp
+++ strelnikov.matvey/S2/datastructs.hpp
@@ -5,6 +5,20 @@
 namespace strelnikov
 {
   template < class T >
+  class Stack
+  {
+  public:
+    void push(const T &);
+    void pop() noexcept;
+    T &get() noexcept;
+    bool empty() const noexcept;
+
+  private:
+    List< T > data_;
+    LIter< T > head_;
+  };
+
+  template < class T >
   class Queue
   {
   public:
@@ -18,15 +32,50 @@ namespace strelnikov
   };
 
   template < class T >
-  class Stack
+  void Stack< T >::push(const T &val)
   {
-  public:
-    void push(const T &);
-    void pop() noexcept;
-    T &get() noexcept;
-    bool empty() const noexcept;
+    return;
+  }
 
-  private:
-    List< T > data_;
-  };
+  template < class T >
+  void Stack< T >::pop() noexcept
+  {
+    return;
+  }
+
+  template < class T >
+  T &Stack< T >::get() noexcept
+  {
+    return T();
+  }
+
+  template < class T >
+  bool Stack< T >::empty() const noexcept
+  {
+    return true;
+  }
+
+  template < class T >
+  void Queue< T >::push(const T &val)
+  {
+    return;
+  }
+
+  template < class T >
+  void Queue< T >::pop() noexcept
+  {
+    return;
+  }
+
+  template < class T >
+  T &Queue< T >::get() noexcept
+  {
+    return T();
+  }
+
+  template < class T >
+  bool Queue< T >::empty() const noexcept
+  {
+    return true;
+  }
 }
