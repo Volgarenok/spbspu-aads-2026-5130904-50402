47285057e8598441e670e0a45c59c9b92f844824
AlisovTimofey <timofeyalisov@gmail.com>
2026-03-26 22:54:47 +0300

    move to dif filles + empty size

--- alisov.timofey/S2/main.cpp
+++ alisov.timofey/S2/main.cpp
@@ -1,24 +1,3 @@
-namespace alisov
-{
-  template < typename T > class Queue
-  {
-  public:
-    void push(T rhs);
-    T drop();
-
-  private:
-    list< T > data_;
-  };
-  template < typename T > class Stack
-  {
-  public:
-    void push(T rhs);
-    T drop();
-
-  private:
-    list< T > data_;
-  };
-}
 int main()
 {
 }
