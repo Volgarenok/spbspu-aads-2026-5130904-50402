badd3c1874893a5ba7016bdb03776b25cbdb8510
sp0146001 <sp0146001@gmail.com>
2026-03-28 00:34:43 +0300

    find operator(if it good)

--- petrov.sasha/S2/expression.hpp
+++ petrov.sasha/S2/expression.hpp
@@ -1,4 +1,14 @@
 #ifndef EXPRESSION_HPP
 #define EXPRESSION_HPP
 
+#include <string>
+#include "stack.hpp"
+
+namespace petrov {
+  bool isOperator(const std::string& token) {
+    bool tok = token == "+" || token == "-" || token == "|" ||
+    tok = token == "%" || token == "/" || token == "*";
+    return tok;
+  }
+}
 #endif
