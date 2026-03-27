9f1241b2cb2c35c216e3b61655817660baa0a06d
sp0146001 <sp0146001@gmail.com>
2026-03-28 00:37:37 +0300

    level of operation

--- petrov.sasha/S2/expression.hpp
+++ petrov.sasha/S2/expression.hpp
@@ -6,9 +6,22 @@
 
 namespace petrov {
   bool isOperator(const std::string& token) {
-    bool tok = token == "+" || token == "-" || token == "|" ||
-    tok = token == "%" || token == "/" || token == "*";
+    bool tok = token == '+' || token == '-' || token == '|' ||
+    tok = token == '%' || token == '/' || token == '*';
     return tok;
+  }
+
+  int getPrecedence(const std::string& op) {
+    if (op == '|') {
+      return 1;
+    }
+    if (op == '+' || op == '-') {
+      return 2;
+    }
+    if (op == '*' || op  == '/' || op == '%') {
+      return 3;
+    }
+    return 0;
   }
 }
 #endif
