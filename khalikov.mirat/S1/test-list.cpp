e7ed1baed9803021fd775d36778b9c7dfb3024a7
khalii21 <miratkhalikov007@gmail.com>
2026-03-27 22:20:11 +0300

    tests added

---
+++ khalikov.mirat/S1/test-list.cpp
@@ -0,0 +1,10 @@
+#include "list.hpp"
+#include <boost/test/unit_test.hpp>
+
+using namespace khalikov;
+
+BOOST_AUTO_TEST_CASE(default_constructor_test)
+{
+	List< int > list;
+	BOOST_TEST(1 == 1);
+}
---
+++ test-list.cpp
@@ -0,0 +1,8 @@
+#include <boost/test/unit_test.hpp>
+#include "list.hpp"
+
+BOOST_AUTO_TEST_CASE(default_constructor_test)
+{
+	List< T > list;
+	BOOST_TEST();
+}
