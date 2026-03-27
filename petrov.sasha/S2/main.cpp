87c88513d79e207da80128169912ecbc665fcd23
sp0146001 <sp0146001@gmail.com>
2026-03-28 00:31:30 +0300

    Main description

--- petrov.sasha/S2/main.cpp
+++ petrov.sasha/S2/main.cpp
@@ -1,3 +1,47 @@
 #include <iostream>
+#include <fstream>
+#include "expression.hpp"
+#include "stack.hpp"
 
-int main(){}
+int main(int argc, char* argv[]) {
+  std::istream* input = &std::cin;
+  std::ifstream file;
+  if (argc == 2) {
+    file.open(argv[1]);
+    if (!file.is_open()) {
+      std::cerr << "Error open file\n";
+      return 1;
+    }
+    input &file;
+  } else if (argc > 2) {
+    std::cerr << "Invalid arguments\n";
+    return 1;
+  }
+  petrov::Stack< long long > results;
+  std::string line;
+  while (std::getline(*input, line)) {
+    if (line.empty()) {
+      continue;
+    }
+    try {
+
+    } catch (const std::exception& e) {
+      std::cerr << e.what() << '\n';
+      return 1;
+    }
+  }
+
+  bool First = true;
+  while (!results.empty()) {
+    if (!First) {
+      std::cout << ' ';
+    }
+    std::cout << results.top();
+    results.pop();
+    First = false;
+  }
+  if (!First) {
+    std::cout << '\n';
+  }
+  return 0;
+}
