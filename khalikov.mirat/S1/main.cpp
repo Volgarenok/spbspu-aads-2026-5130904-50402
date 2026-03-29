9b56503ee7d758210da681e2678d13696c5091c6
khalii21 <miratkhalikov007@gmail.com>
2026-03-29 20:51:27 +0300

    start of S1

--- khalikov.mirat/S1/main.cpp
+++ khalikov.mirat/S1/main.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
 #include "list.hpp"
 
-
 int main()
 {
-
-	khalikov::List< int > l;
-	l.pushFront(4);
-	l.pushFront(2);
-	l.pushFront(3);
-	l.pushFront(11);
-	l.pushFront(5);
-	l.pushFront(4);
-	l.pushFront(7);
-	l.pushFront(2);
-	l.pushFront(2);
-	l.print();
-	l.uniqueSort();
-	l.print();
-	l.reverse();
-	l.print();
+	khalikov::List< size_t > list;
+	list.pushBack(1);
+	list.pushBack(2);
+	list.pushBack(3);
+	list.pushBack(33);
+	list.pushBack(53);
+	using pair_t = std::pair< const char *, khalikov::List< size_t > >;
+	pair_t sequences[]  = {
+		{"first", list}
+	};
+	std::cout << sequences[0].first << '\t';
+	sequences[0].second.print();
+	std::cout << '\n';
 }
