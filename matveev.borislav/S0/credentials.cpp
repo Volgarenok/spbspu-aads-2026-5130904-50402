572993bf07e5be279c0d657b270474e880af67ed
L1sa620 <lisevbom@gmail.com>
2026-04-02 10:22:29 +0300

    Fix CG

--- matveev.borislav/S0/credentials.cpp
+++ matveev.borislav/S0/credentials.cpp
@@ -1,7 +1,7 @@
 #include "credentials.hpp"
 #include <iostream>
 
-std::ostream & matveev::out_credentials(std::ostream &out)
+std::ostream &matveev::out_credentials(std::ostream &out)
 {
   return out << "matveev.borislav";
 }
