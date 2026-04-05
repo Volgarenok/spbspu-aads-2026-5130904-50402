2bc4b90396b89bafdcd8ebe5aa4bef10e91fba12
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-04-05 21:49:07 +0300

    add queue.hpp

---
+++ lachugin.mikhail/S2/queue.hpp
@@ -0,0 +1,49 @@
+#ifndef QUEUE_HPP
+#define QUEUE_HPP
+#include "../common/list.hpp"
+#include <iostream>
+
+namespace lachugin
+{
+  template< typename T > class Queue {
+    List< T > l;
+  public:
+    void push(const T& rhs);
+    void pop();        // удалить
+    T& front();        // получить
+    bool empty() const;
+    size_t size() const;
+  };
+
+  template< typename T >
+  void Queue< T >::pop()
+  {
+    l.popFront();
+  }
+
+  template< typename T >
+  void Queue< T >::push(const T& rhs)
+  {
+    l.add(rhs);
+  }
+
+  template< typename T >
+  T& Queue< T >::front()
+  {
+    return l.front();
+  }
+
+  template< typename T >
+  bool Queue< T >::empty() const
+  {
+    return l.empty();
+  }
+
+  template< typename T >
+  size_t Queue< T >::size() const
+  {
+    return l.size();
+  }
+}
+
+#endif
