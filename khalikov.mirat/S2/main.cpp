96e553d43605404811879c9f9e7f65e63d4979dd
khalii21 <miratkhalikov007@gmail.com>
2026-04-09 22:58:50 +0300

    impl of result

--- khalikov.mirat/S2/main.cpp
+++ khalikov.mirat/S2/main.cpp
@@ -5,8 +5,80 @@
 #include "stack.hpp"
 #include "queue.hpp"
 
-template < class T >
-size_t priority(const T& val);
+long long op(const long long val1, const long long val2, const std::string& operation)
+{
+	long long res = 0;
+	if (operation == "+")
+	{
+		res = val1 + val2;
+	}
+	else if (operation == "-")
+	{
+		res = val1 - val2;
+	}
+	else if (operation == "*")
+	{
+		res = val1 * val2;
+	}
+	else if (operation == "/")
+	{
+		res = val1 / val2;
+	}
+	else if (operation == "#")
+	{
+		std::string value1 = std::to_string(val1);
+		std::string value2 = std::to_string(val2);
+		std::string result = value1 + value2;
+		res = std::stoll(result);
+	}
+	return res;
+}
+
+long long result(khalikov::Queue <std::string>& queue)
+{
+	khalikov::Stack < long long > stack;
+	std::string str;
+	while (!queue.isEmpty())
+	{
+		str += queue.drop() + " ";
+	}
+	std::string el;
+	std::stringstream line(str);
+	while (line >> el)
+	{
+		if (std::isdigit(el[0]) || (el.size() > 1 && el[0] == '-'))
+		{
+			stack.push(std::stoll(el));
+		}
+		else
+		{
+			long long val2 = stack.drop();
+			long long val1 = stack.drop();
+			stack.push(op(val1, val2, el));
+		}
+	}
+	return stack.top();
+}
+
+size_t priority(const std::string& operation)
+{
+	if (operation == "+" || operation == "-")
+	{
+		return 0;
+	}
+	else if (operation == "*" || operation == "/")
+	{
+		return 1;
+	}
+	else if (operation == "#")
+	{
+		return 2;
+	}
+	else
+	{
+		throw std::logic_error("Operation does not exist");
+	}
+}
 
 khalikov::Queue < std::string > transform(const std::string& str)
 {
@@ -47,10 +119,11 @@ khalikov::Queue < std::string > transform(const std::string& str)
 			}
 			else
 			{
-				while (priority(el) > priority(stack.top()) || stack.top() != "(")
+				while (!stack.isEmpty() && stack.top() != "(" && priority(el) <= priority(stack.top()))
 				{
 					queue.push(stack.drop());
 				}
+				stack.push(el);
 			}
 		}
 	}
@@ -69,7 +142,7 @@ void readExp(std::istream& in, std::string& str)
 		{
 			continue;
 		}
-		std::cout << str << '\n';
+		transform
 	}
 }
 
