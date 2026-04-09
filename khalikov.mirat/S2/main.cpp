f5214a97aaf279f9353ffcb05dc058cfb9999275
khalii21 <miratkhalikov007@gmail.com>
2026-04-09 21:45:37 +0300

    transform impl

--- khalikov.mirat/S2/main.cpp
+++ khalikov.mirat/S2/main.cpp
@@ -1,6 +1,65 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
+#include "stack.hpp"
+#include "queue.hpp"
+
+template < class T >
+size_t priority(const T& val);
+
+khalikov::Queue < std::string > transform(const std::string& str)
+{
+	khalikov::Queue < std::string > queue;
+	khalikov::Stack < std::string > stack;
+	std::stringstream line(str);
+	std::string el;
+	while (line >> el)
+	{
+		if (std::isdigit(el[0]) || (el.size() > 1 && el[0] == '-'))
+		{
+			queue.push(el);
+		}
+		else if (el == "(")
+		{
+			stack.push(el);
+		}
+		else if (el == ")")
+		{
+			while (!stack.isEmpty() && stack.top() != "(")
+			{
+				queue.push(stack.drop());
+			}
+			if (!stack.isEmpty())
+			{
+				stack.pop();
+			}
+		}
+		else
+		{
+			if (stack.isEmpty() || stack.top() == "(")
+			{
+				stack.push(el);
+			}
+			else if (priority(el) > priority(stack.top()))
+			{
+				stack.push(el);
+			}
+			else
+			{
+				while (priority(el) > priority(stack.top()) || stack.top() != "(")
+				{
+					queue.push(stack.drop());
+				}
+			}
+		}
+	}
+	while (!stack.isEmpty())
+	{
+		queue.push(stack.drop());
+	}
+	return queue;
+}
 
 void readExp(std::istream& in, std::string& str)
 {
