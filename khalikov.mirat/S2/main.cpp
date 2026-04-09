36777195d46254e808cad6b67e7c1fd6f32ac907
khalii21 <miratkhalikov007@gmail.com>
2026-04-09 17:45:46 +0300

    impl of readExp

--- khalikov.mirat/S2/main.cpp
+++ khalikov.mirat/S2/main.cpp
@@ -1,2 +1,34 @@
-int main()
-{}
+#include <iostream>
+#include <string>
+#include <fstream>
+
+void readExp(std::istream& in, std::string& str)
+{
+	while (std::getline(in, str))
+	{
+		if (str.empty())
+		{
+			continue;
+		}
+		std::cout << str << '\n';
+	}
+}
+
+int main(int argc, char ** argv)
+{
+	std::string str;
+	if (argc == 2)
+	{
+		std::ifstream inputFile(argv[1]);
+		if (!inputFile.is_open())
+		{
+			std::cerr << "Read error" << '\n';
+			return 1;
+		}
+		readExp(inputFile, str);
+	}
+	else
+	{
+		readExp(std::cin, str);
+	}
+}
