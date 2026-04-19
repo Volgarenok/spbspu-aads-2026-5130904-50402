668e5dc9303f79720e942b19289c87044f2d30e8
voyadger01 <voyadgerkx@gmail.com>
2026-04-19 11:30:15 +0300

    from begin/end to cbegin/cend

--- karpovich.dmitriy/S1/process.cpp
+++ karpovich.dmitriy/S1/process.cpp
@@ -1,10 +1,10 @@
+#include "process.hpp"
 #include <cstddef>
+#include <iterators.hpp>
 #include <limits>
+#include <list.hpp>
 #include <stdexcept>
 #include <utility>
-#include <iterators.hpp>
-#include <list.hpp>
-#include "process.hpp"
 
 void karpovich::input(std::istream &in, list_pair_t &list)
 {
@@ -23,16 +23,16 @@ void karpovich::input(std::istream &in, list_pair_t &list)
 void karpovich::transpose(const list_pair_t &list, List< List< size_t > > &data)
 {
   size_t max_len = 0;
-  for (LCIter< std::pair< std::string, List< size_t > > > it = list.begin(); it != list.end(); it++) {
+  for (LCIter< std::pair< std::string, List< size_t > > > it = list.cbegin(); it != list.cend(); ++it) {
     if ((*it).second.size() > max_len) {
       max_len = (*it).second.size();
     }
   }
   for (size_t i = 0; i < max_len; i++) {
     List< size_t > row_numbers;
-    for (LCIter< std::pair< std::string, List< size_t > > > seq_it = list.begin(); seq_it != list.end(); seq_it++) {
+    for (LCIter< std::pair< std::string, List< size_t > > > seq_it = list.cbegin(); seq_it != list.cend(); ++seq_it) {
       if (i < (*seq_it).second.size()) {
-        LCIter< size_t > num_it = (*seq_it).second.begin();
+        LCIter< size_t > num_it = (*seq_it).second.cbegin();
         for (size_t j = 0; j < i; ++j) {
           ++num_it;
         }
@@ -48,7 +48,7 @@ void karpovich::transpose(const list_pair_t &list, List< List< size_t > > &data)
 void karpovich::printNames(std::ostream &out, const list_pair_t &names)
 {
   bool first = true;
-  for (LCIter< std::pair< std::string, List< size_t > > > it = names.begin(); it != names.end(); ++it) {
+  for (LCIter< std::pair< std::string, List< size_t > > > it = names.cbegin(); it != names.cend(); ++it) {
     if (!first) {
       out << ' ';
     }
@@ -61,9 +61,9 @@ void karpovich::printNames(std::ostream &out, const list_pair_t &names)
 void karpovich::printTransposed(std::ostream &out, const List< List< size_t > > &transposed)
 {
   bool first = true;
-  for (LCIter< List< size_t > > row_it = transposed.begin(); row_it != transposed.end(); ++row_it) {
+  for (LCIter< List< size_t > > row_it = transposed.cbegin(); row_it != transposed.cend(); ++row_it) {
     first = true;
-    for (LCIter< size_t > num_it = (*row_it).begin(); num_it != (*row_it).end(); ++num_it) {
+    for (LCIter< size_t > num_it = (*row_it).cbegin(); num_it != (*row_it).cend(); ++num_it) {
       if (!first) {
         out << ' ';
       }
@@ -83,9 +83,9 @@ void karpovich::output(std::ostream &out, const list_pair_t &names, const List< 
     out << "0\n";
   } else {
     List< size_t > sums;
-    for (LCIter< List< size_t > > row_it = transposed.begin(); row_it != transposed.end(); ++row_it) {
+    for (LCIter< List< size_t > > row_it = transposed.cbegin(); row_it != transposed.cend(); ++row_it) {
       size_t sum = 0;
-      for (LCIter< size_t > num_it = (*row_it).begin(); num_it != (*row_it).end(); ++num_it) {
+      for (LCIter< size_t > num_it = (*row_it).cbegin(); num_it != (*row_it).cend(); ++num_it) {
         if (*num_it > std::numeric_limits< size_t >::max() - sum) {
           throw std::overflow_error("overflow");
           return;
@@ -94,7 +94,7 @@ void karpovich::output(std::ostream &out, const list_pair_t &names, const List< 
       }
       sums.push_back(sum);
     }
-    for (LIter< size_t > it = sums.begin(); it != sums.end(); it++) {
+    for (LIter< size_t > it = sums.begin(); it != sums.end(); ++it) {
       if (!first) {
         out << ' ';
       }
--- karpovich.dmitriy/S1/test-list.cpp
+++ karpovich.dmitriy/S1/test-list.cpp
@@ -1,6 +1,6 @@
-#include <boost/test/unit_test.hpp>
 #include <iterators.hpp>
 #include <list.hpp>
+#include <boost/test/unit_test.hpp>
 
 using namespace karpovich;
 
