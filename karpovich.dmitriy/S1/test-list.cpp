2fcda380d2cc6fcacdc57cf8207857499d6965ad
voyadger01 <voyadgerkx@gmail.com>
2026-04-19 11:39:15 +0300

    fix test with cbegin/cend

--- karpovich.dmitriy/S1/test-list.cpp
+++ karpovich.dmitriy/S1/test-list.cpp
@@ -90,13 +90,13 @@ BOOST_AUTO_TEST_CASE(test_end)
 BOOST_AUTO_TEST_CASE(test_begin_const)
 {
   const List< int > list;
-  BOOST_CHECK(list.begin() == list.end());
+  BOOST_CHECK(list.cbegin() == list.cend());
 }
 
 BOOST_AUTO_TEST_CASE(test_end_const)
 {
   const List< int > list;
-  BOOST_CHECK(list.begin() == list.end());
+  BOOST_CHECK(list.cbegin() == list.cend());
 }
 
 BOOST_AUTO_TEST_CASE(test_push_front)
