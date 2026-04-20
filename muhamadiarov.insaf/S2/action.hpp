28c8141ac777835394f625f0eeb8776676a97247
InsafM1 <muhamadiarovinsaf0@gmail.com>
2026-04-20 10:50:17 +0300

    reduced the line size

--- muhamadiarov.insaf/S2/action.hpp
+++ muhamadiarov.insaf/S2/action.hpp
@@ -9,20 +9,21 @@
 
 namespace muhamadiarov
 {
-  long long sum(long long a, long long b);
-  long long div(long long a, long long b);
-  long long sub(long long a, long long b);
-  long long mult(long long a, long long b);
-  long long mod(long long a, long long b);
-  long long xcor(long long a, long long b);
-  const std::pair< std::string, long long (*)(long long, long long)> oprts[] { {"^", xcor}, {"+", sum}, {"-", sub}, {"*", mult}, {"/", div}, {"%", mod} };
+  using l_d = long long;
+  l_d sum(l_d a, l_d b);
+  l_d div(l_d a, l_d b);
+  l_d sub(l_d a, l_d b);
+  l_d mult(l_d a, l_d b);
+  l_d mod(l_d a, l_d b);
+  l_d xcor(l_d a, l_d b);
+  const std::pair< std::string, l_d (*)(l_d, l_d)> oprts[] { {"^", xcor}, {"+", sum}, {"-", sub}, {"*", mult}, {"/", div}, {"%", mod} };
   Queque< Queque < std::string > > input(std::istream& in);
   Queque< std::string > divideString(const std::string str);
   std::istream& getLine(std::istream& in, std::string& str);
-  long long calcExpr(Queque< std::string >& expr);
+  l_d calcExpr(Queque< std::string >& expr);
   bool isPriority(const std::string op1, const std::string op2);
   bool isDigit(const std::string str);
-  long long calc(long long a, long long b, const std::string op);
+  l_d calc(l_d a, l_d b, const std::string op);
   bool isTrueOprt(const std::string op);
 }
 #endif
