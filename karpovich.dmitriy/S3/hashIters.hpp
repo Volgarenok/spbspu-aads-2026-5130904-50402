235ff1dbf763a6cb50daad8c3976d69f4997f2cd
voyadger01 <voyadgerkx@gmail.com>
2026-05-03 21:08:29 +0300

    decl of hash table and iters

---
+++ karpovich.dmitriy/S3/hashIters.hpp
@@ -0,0 +1,14 @@
+#ifndef HASH_ITERS_HPP
+#define HASH_ITERS_HPP
+template < class Key, class Value, class Hash, class Equal >
+class HashIter
+{
+
+};
+template < class Key, class Value, class Hash, class Equal >
+class HashConstIter
+{
+
+};
+
+#endif
---
+++ karpovich.dmitriy/S3/hashTable.hpp
@@ -0,0 +1,53 @@
+#ifndef HASH_TABLE_HPP
+#define HASH_TABLE_HPP
+#include <cstddef>
+#include <utility>
+#include "../common/list.hpp"
+#include "hashIters.hpp"
+namespace karpovich
+{
+
+  template < class Key, class Value, class Hash, class Equal >
+  class HashTable
+  {
+    friend class HashIter< Key, Value, Hash, Equal >;
+    friend class HashConstIter< Key, Value, Hash, Equal >;
+    using value_type = std::pair< Key, Value >;
+
+  public:
+    using HIter = HashIter< Key, Value, Hash, Equal >;
+    using HCIter = HashConstIter< Key, Value, Hash, Equal >;
+
+    HashTable(std::size_t slots);
+    ~HashTable();
+
+    HashTable(const HashTable &other);
+    HashTable(HashTable &&other) noexcept;
+    HashTable &operator=(const HashTable &other);
+    HashTable &operator=(HashTable &&other) noexcept;
+
+    void add(Key k, Value v);
+    Value drop(Key k);
+    Value get(Key k) const;
+    bool has(Key k) const noexcept;
+    void rehash(size_t slots);
+
+    void clear() noexcept;
+    size_t size() const noexcept;
+    bool empty() const noexcept;
+    void swap(HashTable &other) noexcept;
+
+    HIter begin();
+    HIter end();
+    HCIter begin() const;
+    HCIter end() const;
+
+  private:
+    List< value_type > *buckets_;
+    size_t capacity_;
+    size_t size_;
+    Hash hasher_;
+    Equal comparator_;
+  };
+}
+#endif
