3536db50056cbf220c55dec6afa9edabcd9261f7
voyadger01 <voyadgerkx@gmail.com>
2026-05-04 01:29:25 +0300

    add merge extract

--- karpovich.dmitriy/S3/commands.cpp
+++ karpovich.dmitriy/S3/commands.cpp
@@ -3,7 +3,6 @@
 
 namespace details
 {
-
   void sortStrings(karpovich::Vector< std::string > &vec)
   {
     for (size_t i = 1; i < vec.getSize(); ++i) {
@@ -16,7 +15,6 @@ namespace details
       vec[j] = key;
     }
   }
-
   void sortWeights(karpovich::Vector< size_t > &vec)
   {
     for (size_t i = 1; i < vec.getSize(); ++i) {
@@ -29,7 +27,6 @@ namespace details
       vec[j] = key;
     }
   }
-
 }
 
 void karpovich::cmdGraphs(std::istream &, std::ostream &out, karpovich::GraphSet &graphs)
@@ -182,4 +179,86 @@ void karpovich::cmdInbound(std::istream &in, std::ostream &out, karpovich::Graph
     }
     out << "\n";
   }
+}
+
+void karpovich::cmdMerge(std::istream &in, std::ostream &out, karpovich::GraphSet &graphs)
+{
+  std::string new_g, g1, g2;
+  in >> new_g >> g1 >> g2;
+  (void)out;
+  if (graphs.has(new_g) || !graphs.has(g1) || !graphs.has(g2)) {
+    throw std::runtime_error("Invalid");
+  }
+  karpovich::Graph merged(16);
+  karpovich::Graph &src1 = graphs.get(g1);
+  karpovich::Graph &src2 = graphs.get(g2);
+  for (auto it = src1.vertices.begin(); it != src1.vertices.end(); ++it) {
+    merged.addVertex(*it);
+  }
+  for (auto it = src2.vertices.begin(); it != src2.vertices.end(); ++it) {
+    merged.addVertex(*it);
+  }
+  for (auto it = src1.edges.begin(); it != src1.edges.end(); ++it) {
+    for (auto wit = (*it).second.begin(); wit != (*it).second.end(); ++wit) {
+      merged.addEdge((*it).first.first, (*it).first.second, *wit);
+    }
+  }
+  for (auto it = src2.edges.begin(); it != src2.edges.end(); ++it) {
+    for (auto wit = (*it).second.begin(); wit != (*it).second.end(); ++wit) {
+      merged.addEdge((*it).first.first, (*it).first.second, *wit);
+    }
+  }
+  graphs.add(new_g, std::move(merged));
+}
+
+void karpovich::cmdExtract(std::istream &in, std::ostream &out, karpovich::GraphSet &graphs)
+{
+  std::string new_g, old_g;
+  size_t k = 0;
+  in >> new_g >> old_g >> k;
+  (void)out;
+  if (graphs.has(new_g) || !graphs.has(old_g)) {
+    throw std::runtime_error("Invalid");
+  }
+  karpovich::Graph &src = graphs.get(old_g);
+  karpovich::List< std::string > required;
+  for (size_t i = 0; i < k; ++i) {
+    std::string v;
+    in >> v;
+    required.push_back(v);
+  }
+  for (auto it = required.begin(); it != required.end(); ++it) {
+    bool found = false;
+    for (auto vit = src.vertices.begin(); vit != src.vertices.end(); ++vit) {
+      if (*vit == *it) {
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      throw std::out_of_range("Vertex missing");
+    }
+  }
+  karpovich::Graph ext(16);
+  for (auto it = required.begin(); it != required.end(); ++it) {
+    ext.addVertex(*it);
+  }
+  for (auto it = src.edges.begin(); it != src.edges.end(); ++it) {
+    bool v1_ok = false;
+    bool v2_ok = false;
+    for (auto rit = required.begin(); rit != required.end(); ++rit) {
+      if (*rit == (*it).first.first) {
+        v1_ok = true;
+      }
+      if (*rit == (*it).first.second) {
+        v2_ok = true;
+      }
+    }
+    if (v1_ok && v2_ok) {
+      for (auto wit = (*it).second.begin(); wit != (*it).second.end(); ++wit) {
+        ext.addEdge((*it).first.first, (*it).first.second, *wit);
+      }
+    }
+  }
+  graphs.add(new_g, std::move(ext));
 }
