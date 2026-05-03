73c7a8efe3a6c70ec7691067593650b3387f5ef4
voyadger01 <voyadgerkx@gmail.com>
2026-05-04 01:05:18 +0300

    add decl of cli cmds

---
+++ karpovich.dmitriy/S3/commands.hpp
@@ -0,0 +1,30 @@
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include "graph.hpp"
+#include "hashFunctions.hpp"
+#include "hashTable.hpp"
+
+namespace karpovich
+{
+
+  using GraphSet = HashTable < std::string, Graph, Hasher< std::string, boost::hash2::siphash_64 >,
+        std::equal_to< std::string >;
+
+  void cmdGraphs(std::istream &in, std::ostream &out, GraphSet &graphs);
+  void cmdVertexes(std::istream &in, std::ostream &out, GraphSet &graphs);
+  void cmdOutbound(std::istream &in, std::ostream &out, GraphSet &graphs);
+  void cmdInbound(std::istream &in, std::ostream &out, GraphSet &graphs);
+  void cmdBind(std::istream &in, std::ostream &out, GraphSet &graphs);
+  void cmdCut(std::istream &in, std::ostream &out, GraphSet &graphs);
+  void cmdCreate(std::istream &in, std::ostream &out, GraphSet &graphs);
+  void cmdMerge(std::istream &in, std::ostream &out, GraphSet &graphs);
+  void cmdExtract(std::istream &in, std::ostream &out, GraphSet &graphs);
+
+}
+
+#endif
