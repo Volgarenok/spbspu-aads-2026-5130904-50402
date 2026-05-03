82e3f65800f4a281171683886933b91ca7b28b7e
voyadger01 <voyadgerkx@gmail.com>
2026-05-04 01:26:34 +0300

    add inbound outbound

--- karpovich.dmitriy/S3/commands.cpp
+++ karpovich.dmitriy/S3/commands.cpp
@@ -93,3 +93,93 @@ void karpovich::cmdCreate(std::istream &in, std::ostream &, karpovich::GraphSet 
   }
   graphs.add(g_name, std::move(g));
 }
+
+void karpovich::cmdOutbound(std::istream &in, std::ostream &out, karpovich::GraphSet &graphs)
+{
+  std::string g_name, v_name;
+  in >> g_name >> v_name;
+  karpovich::Graph &g = graphs.get(g_name);
+  bool v_exists = false;
+  for (auto it = g.vertices.begin(); it != g.vertices.end(); ++it) {
+    if (*it == v_name) {
+      v_exists = true;
+      break;
+    }
+  }
+  if (!v_exists) {
+    throw std::out_of_range("Vertex missing");
+  }
+  karpovich::List< std::pair< std::string, karpovich::List< size_t > > > out_edges;
+  for (auto it = g.edges.begin(); it != g.edges.end(); ++it) {
+    if ((*it).first.first == v_name) {
+      out_edges.push_back(std::make_pair((*it).first.second, (*it).second));
+    }
+  }
+  karpovich::Vector< std::string > dests;
+  for (auto it = out_edges.begin(); it != out_edges.end(); ++it) {
+    dests.pushBack((*it).first);
+  }
+  details::sortStrings(dests);
+  for (size_t i = 0; i < dests.getSize(); ++i) {
+    out << dests[i];
+    for (auto it = out_edges.begin(); it != out_edges.end(); ++it) {
+      if ((*it).first == dests[i]) {
+        karpovich::Vector< size_t > ws;
+        for (auto wit = (*it).second.begin(); wit != (*it).second.end(); ++wit) {
+          ws.pushBack(*wit);
+        }
+        details::sortWeights(ws);
+        for (size_t j = 0; j < ws.getSize(); ++j) {
+          out << " " << ws[j];
+        }
+        break;
+      }
+    }
+    out << "\n";
+  }
+}
+
+void karpovich::cmdInbound(std::istream &in, std::ostream &out, karpovich::GraphSet &graphs)
+{
+  std::string g_name, v_name;
+  in >> g_name >> v_name;
+  karpovich::Graph &g = graphs.get(g_name);
+  bool v_exists = false;
+  for (auto it = g.vertices.begin(); it != g.vertices.end(); ++it) {
+    if (*it == v_name) {
+      v_exists = true;
+      break;
+    }
+  }
+  if (!v_exists) {
+    throw std::out_of_range("Vertex missing");
+  }
+  karpovich::List< std::pair< std::string, karpovich::List< size_t > > > in_edges;
+  for (auto it = g.edges.begin(); it != g.edges.end(); ++it) {
+    if ((*it).first.second == v_name) {
+      in_edges.push_back(std::make_pair((*it).first.first, (*it).second));
+    }
+  }
+  karpovich::Vector< std::string > srcs;
+  for (auto it = in_edges.begin(); it != in_edges.end(); ++it) {
+    srcs.pushBack((*it).first);
+  }
+  details::sortStrings(srcs);
+  for (size_t i = 0; i < srcs.getSize(); ++i) {
+    out << srcs[i];
+    for (auto it = in_edges.begin(); it != in_edges.end(); ++it) {
+      if ((*it).first == srcs[i]) {
+        karpovich::Vector< size_t > ws;
+        for (auto wit = (*it).second.begin(); wit != (*it).second.end(); ++wit) {
+          ws.pushBack(*wit);
+        }
+        details::sortWeights(ws);
+        for (size_t j = 0; j < ws.getSize(); ++j) {
+          out << " " << ws[j];
+        }
+        break;
+      }
+    }
+    out << "\n";
+  }
+}
