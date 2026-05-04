7d2f5e78b64ff97c6d28ba0a2c07e94fadddb048
L1sa620 <lisevbom@gmail.com>
2026-05-04 21:18:54 +0300

    Implementation hash table basics

--- matveev.borislav/S3/hash-table.hpp
+++ matveev.borislav/S3/hash-table.hpp
@@ -4,12 +4,14 @@
 #include <cstddef>
 #include <stdexcept>
 #include <utility>
+#include <limits>
 
 namespace matveev
 {
 template< class Key, class Value >
 struct HashTableItem
 {
+  HashTableItem();
   Key key;
   Value value;
   bool occupied;
@@ -46,7 +48,73 @@ private:
   Equal equal_;
 
   size_t indexOf(const Key& key) const;
+  size_t capacity() const noexcept;
 };
+
+template< class Key, class Value >
+HashTableItem< Key, Value >::HashTableItem():
+  key(),
+  value(),
+  occupied(false)
+{}
+
+template< class Key, class Value, class Hash, class Equal >
+HashTable< Key, Value, Hash, Equal >::HashTable(size_t buckets, size_t bucket_capacity):
+  data_(nullptr),
+  bucket_count_(buckets),
+  bucket_capacity_(bucket_capacity),
+  size_(0),
+  hash_(),
+  equal_()
+{
+  if (bucket_count_ == 0 || bucket_capacity_ == 0)
+  {
+    throw std::invalid_argument("invalid hash table capacity");
+  }
+
+  if (bucket_count_ > std::numeric_limits< size_t >::max() / bucket_capacity_)
+  {
+    throw std::overflow_error("hash table capacity overflow");
+  }
+
+  data_ = new HashTableItem< Key, Value >[capacity()];
+}
+
+template< class Key, class Value, class Hash, class Equal >
+HashTable< Key, Value, Hash, Equal >::~HashTable()
+{
+  delete[] data_;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+size_t HashTable< Key, Value, Hash, Equal >::size() const noexcept
+{
+  return size_;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+size_t HashTable< Key, Value, Hash, Equal >::bucketCount() const noexcept
+{
+  return bucket_count_;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+size_t HashTable< Key, Value, Hash, Equal >::bucketCapacity() const noexcept
+{
+  return bucket_capacity_;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+size_t HashTable< Key, Value, Hash, Equal >::indexOf(const Key& key) const
+{
+  return hash_(key) % bucket_count_;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+size_t HashTable< Key, Value, Hash, Equal >::capacity() const noexcept
+{
+  return bucket_count_ * bucket_capacity_;
+}
 
 }
 
--- matveev.borislav/S3/main.cpp
+++ matveev.borislav/S3/main.cpp
@@ -1,5 +1,34 @@
+#include "hash-table.hpp"
+
+#include <cstddef>
 #include <iostream>
+
+namespace
+{
+struct IntHash
+{
+  size_t operator()(int value) const
+  {
+    return static_cast< size_t >(value);
+  }
+};
+
+struct IntEqual
+{
+  bool operator()(int lhs, int rhs) const
+  {
+    return lhs == rhs;
+  }
+};
+}
 
 int main()
 {
+  matveev::HashTable< int, int, IntHash, IntEqual > table(5, 2);
+
+  std::cout << table.size() << '\n';
+  std::cout << table.bucketCount() << '\n';
+  std::cout << table.bucketCapacity() << '\n';
+
+  return 0;
 }
