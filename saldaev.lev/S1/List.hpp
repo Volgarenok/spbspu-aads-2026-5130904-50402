14a9df2177bac47eaf9e80e0d86b9ed17dffc78c
Carrot_lord <lev.saldaev@gmai.com>
2026-05-04 19:26:10 +0300

    Refactor List: rename methods, nest iterators, update friendship

--- saldaev.lev/S1/List.hpp
+++ saldaev.lev/S1/List.hpp
@@ -5,14 +5,58 @@
 namespace saldaev
 {
   template < class T >
-  struct LCIter;
-  template < class T >
-  struct LIter;
-  template < class T >
   struct List
   {
-    friend LCIter< T >;
-    friend LIter< T >;
+    struct LCIter
+    {
+      friend class List< T >;
+      friend struct LIter< T >;
+
+      bool isValid() const noexcept;
+      bool hasNext() const noexcept;
+      bool hasPrev() const noexcept;
+      LCIter< T > &operator++() noexcept;
+      LCIter< T > operator++(int) noexcept;
+      LCIter< T > &operator--() noexcept;
+      LCIter< T > operator--(int) noexcept;
+      bool operator==(const LCIter &other) const noexcept;
+      bool operator==(const LIter< T > &other) const noexcept;
+      bool operator!=(const LCIter &other) const noexcept;
+      bool operator!=(const LIter< T > &other) const noexcept;
+      const T &getData() const noexcept;
+
+    private:
+      typename List< T >::Node *curr;
+      bool valid;
+
+      explicit LCIter(typename List< T >::Node *node);
+    };
+
+    struct LIter
+    {
+      friend class List< T >;
+      friend struct LCIter< T >;
+
+      bool isValid() const noexcept;
+      bool hasNext() const noexcept;
+      bool hasPrev() const noexcept;
+      LIter< T > &operator++() noexcept;
+      LIter< T > operator++(int) noexcept;
+      LIter< T > &operator--() noexcept;
+      LIter< T > operator--(int) noexcept;
+      bool operator==(const LIter &other) const noexcept;
+      bool operator==(const LCIter< T > &other) const noexcept;
+      bool operator!=(const LIter &other) const noexcept;
+      bool operator!=(const LCIter< T > &other) const noexcept;
+      const T &getData() const noexcept;
+      T &getData() noexcept;
+
+    private:
+      typename List< T >::Node *curr;
+      bool valid;
+
+      explicit LIter(typename List< T >::Node *node);
+    };
 
     List();
     ~List();
@@ -21,24 +65,24 @@ namespace saldaev
     List(List &&other);
     List &operator=(List &&other);
 
-    void newHead(const T &d);
-    void newTail(const T &d);
-    void cutHead() noexcept;
-    void cutTail() noexcept;
+    void pushFront(const T &d);
+    void pushBack(const T &d);
+    T popFront() noexcept;
+    T popBack() noexcept;
     void clear() noexcept;
     void swap(List &other) noexcept;
 
     void setData(LIter< T > it, const T &d);
-    void addBefore(LIter< T > it, const T &d);
-    void addAfter(LIter< T > it, const T &d);
-    LIter< T > cutCurrent(LIter< T > it) noexcept;
+    void insertBefore(LIter< T > it, const T &d);
+    void insertAfter(LIter< T > it, const T &d);
+    LIter< T > erase(LIter< T > it) noexcept;
 
     LIter< T > begin();
     LIter< T > end();
     LCIter< T > begin() const;
     LCIter< T > end() const;
 
-    size_t getLength() const noexcept;
+    size_t size() const noexcept;
 
   private:
     struct Node
@@ -53,59 +97,6 @@ namespace saldaev
     Node *head;
     Node *tail;
     size_t length;
-  };
-
-  template < class T >
-  struct LCIter
-  {
-    friend class List< T >;
-    friend struct LIter< T >;
-
-    bool isValid() const noexcept;
-    bool hasNext() const noexcept;
-    bool hasPrev() const noexcept;
-    LCIter< T > &operator++() noexcept;
-    LCIter< T > operator++(int) noexcept;
-    LCIter< T > &operator--() noexcept;
-    LCIter< T > operator--(int) noexcept;
-    bool operator==(const LCIter &other) const noexcept;
-    bool operator==(const LIter< T > &other) const noexcept;
-    bool operator!=(const LCIter &other) const noexcept;
-    bool operator!=(const LIter< T > &other) const noexcept;
-    const T &getData() const noexcept;
-
-  private:
-    typename List< T >::Node *curr;
-    bool valid;
-
-    explicit LCIter(typename List< T >::Node *node);
-  };
-
-  template < class T >
-  struct LIter
-  {
-    friend class List< T >;
-    friend struct LCIter< T >;
-
-    bool isValid() const noexcept;
-    bool hasNext() const noexcept;
-    bool hasPrev() const noexcept;
-    LIter< T > &operator++() noexcept;
-    LIter< T > operator++(int) noexcept;
-    LIter< T > &operator--() noexcept;
-    LIter< T > operator--(int) noexcept;
-    bool operator==(const LIter &other) const noexcept;
-    bool operator==(const LCIter< T > &other) const noexcept;
-    bool operator!=(const LIter &other) const noexcept;
-    bool operator!=(const LCIter< T > &other) const noexcept;
-    const T &getData() const noexcept;
-    T &getData() noexcept;
-
-  private:
-    typename List< T >::Node *curr;
-    bool valid;
-
-    explicit LIter(typename List< T >::Node *node);
   };
 }
 
