d48270dd1caba29580ec02812d0f5b7f936b3406
BukreevYas <bukreev.yas@edu.spbstu.ru>
2026-05-04 16:53:21 -0400

    Add tests for stack

---
+++ bukreev.yakov/S2/test-stack.cpp
@@ -0,0 +1,31 @@
+#include <boost/test/unit_test.hpp>
+#include "stack.hpp"
+
+using namespace bukreev;
+
+BOOST_AUTO_TEST_SUITE(StackTests)
+
+BOOST_AUTO_TEST_CASE(test_push)
+{
+  Stack< int > stack;
+  stack.push(1);
+  stack.push(2);
+
+  BOOST_CHECK_EQUAL(stack.size(), 2);
+}
+
+BOOST_AUTO_TEST_CASE(test_pop)
+{
+  Stack< int > stack;
+  stack.push(1);
+  stack.push(2);
+  int popped = stack.pop();
+
+  BOOST_CHECK_EQUAL(stack.size(), 1);
+  BOOST_CHECK_EQUAL(popped, 2);
+  popped = stack.pop();
+  BOOST_CHECK_EQUAL(stack.size(), 0);
+  BOOST_CHECK_EQUAL(popped, 1);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
