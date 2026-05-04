7627cead10c863cb5fb5f93410061b888abfbc55
Carrot_lord <lev.saldaev@gmai.com>
2026-05-04 19:35:45 +0300

    Refactor List iterators to standard pointer-like interface

--- saldaev.lev/S1/List.hpp
+++ saldaev.lev/S1/List.hpp
@@ -9,51 +9,43 @@ namespace saldaev
   {
     struct LCIter
     {
-      friend class List< T >;
-      friend struct LIter< T >;
+      friend class List;
 
-      bool isValid() const noexcept;
-      bool hasNext() const noexcept;
-      bool hasPrev() const noexcept;
       LCIter< T > &operator++() noexcept;
       LCIter< T > operator++(int) noexcept;
       LCIter< T > &operator--() noexcept;
       LCIter< T > operator--(int) noexcept;
       bool operator==(const LCIter &other) const noexcept;
-      bool operator==(const LIter< T > &other) const noexcept;
       bool operator!=(const LCIter &other) const noexcept;
-      bool operator!=(const LIter< T > &other) const noexcept;
-      const T &getData() const noexcept;
+
+      const T &operator*() const;
+      const T *operator->() const;
 
     private:
       typename List< T >::Node *curr;
-      bool valid;
 
       explicit LCIter(typename List< T >::Node *node);
     };
 
     struct LIter
     {
-      friend class List< T >;
-      friend struct LCIter< T >;
+      friend class List;
 
-      bool isValid() const noexcept;
-      bool hasNext() const noexcept;
-      bool hasPrev() const noexcept;
       LIter< T > &operator++() noexcept;
       LIter< T > operator++(int) noexcept;
       LIter< T > &operator--() noexcept;
       LIter< T > operator--(int) noexcept;
+
       bool operator==(const LIter &other) const noexcept;
-      bool operator==(const LCIter< T > &other) const noexcept;
       bool operator!=(const LIter &other) const noexcept;
-      bool operator!=(const LCIter< T > &other) const noexcept;
-      const T &getData() const noexcept;
-      T &getData() noexcept;
+
+      T &operator*();
+      T *operator->();
+      const T &operator*() const;
+      const T *operator->() const;
 
     private:
       typename List< T >::Node *curr;
-      bool valid;
 
       explicit LIter(typename List< T >::Node *node);
     };
@@ -67,12 +59,11 @@ namespace saldaev
 
     void pushFront(const T &d);
     void pushBack(const T &d);
-    T popFront() noexcept;
-    T popBack() noexcept;
+    T popFront();
+    T popBack();
     void clear() noexcept;
     void swap(List &other) noexcept;
 
-    void setData(LIter< T > it, const T &d);
     void insertBefore(LIter< T > it, const T &d);
     void insertAfter(LIter< T > it, const T &d);
     LIter< T > erase(LIter< T > it) noexcept;
