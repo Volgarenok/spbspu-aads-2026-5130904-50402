bd70341b4362e40165a97ba7b1a69e00cad967c3
jar <Jarafan3@gmail.com>
2026-05-04 18:05:29 +0300

    Add files

--- afanasev.yaroslav/S3/main.cpp
+++ afanasev.yaroslav/S3/main.cpp
@@ -4,12 +4,12 @@ int main(int argc, char* argv[])
 {
   if (argc != 2)
   {
-    std::cerr << "Ошибка: не указано имя файла.\n";
+    std::cerr << "I need filename\n";
     return 1;
   }
 
   const char * filename = argv[1];
-  std::cout << "Имя файла: " << filename << std::endl;
+  std::cout << "Имя файла: " << filename << '\n';
 
   return 0;
 }
