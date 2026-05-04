896b75bad4c49bfcdec163ada8a6bc0df1fc9fac
jar <Jarafan3@gmail.com>
2026-05-04 17:18:09 +0300

    Add getting filename

--- afanasev.yaroslav/S3/main.cpp
+++ afanasev.yaroslav/S3/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
-  
+  if (argc != 2)
+  {
+    std::cerr << "Ошибка: не указано имя файла.\n";
+    return 1;
+  }
+
+  const char * filename = argv[1];
+  std::cout << "Имя файла: " << filename << std::endl;
+
+  return 0;
 }
