dbe8bed9ef43fb9c599a171e8c9cfd91b106cbad
voyadger01 <voyadgerkx@gmail.com>
2026-05-05 21:29:08 +0300

    iterators decl

--- karpovich.dmitriy/S4/bstiterators.hpp
+++ karpovich.dmitriy/S4/bstiterators.hpp
@@ -13,11 +13,35 @@ namespace karpovich
   template < class Key, class Value >
   class BSTConstIterator
   {
+  public:
+    const std::pair< const Key, Value > &operator*() const;
+    BSTConstIterator &operator++();
+    BSTConstIterator operator++(int);
+    BSTConstIterator &operator--();
+    BSTConstIterator operator--(int);
+    bool operator==(const BSTConstIterator &other) const;
+    bool operator!=(const BSTConstIterator &other) const;
+    friend class BSTree< Key, Value, class Compare >;
+
+  private:
+    TreeNode< Key, Value > *node_;
   };
 
   template < class Key, class Value >
   class BSTIterator
   {
+  public:
+    std::pair< const Key, Value > &operator*() const;
+    BSTIterator &operator++();
+    BSTIterator operator++(int);
+    BSTIterator &operator--();
+    BSTIterator operator--(int);
+    bool operator==(const BSTIterator &other) const;
+    bool operator!=(const BSTIterator &other) const;
+    friend class BSTree< Key, Value, class Compare >;
+
+  private:
+    TreeNode< Key, Value > *node_;
   };
 }
 
