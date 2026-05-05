9dfb5a31d54f11d5f4ca415a63f2396d5856ec90
voyadger01 <voyadgerkx@gmail.com>
2026-05-06 00:03:41 +0300

    add union

--- karpovich.dmitriy/S4/commands.cpp
+++ karpovich.dmitriy/S4/commands.cpp
@@ -74,3 +74,32 @@ void karpovich::cmdIntersect(std::istream &in, std::ostream &out, karpovich::Dat
   }
   datasets.push(new_name, std::move(new_ds));
 }
+
+void karpovich::cmdUnion(std::istream &in, std::ostream &out, karpovich::Datasets &datasets)
+{
+  std::string new_name, name1, name2;
+  if (!(in >> new_name >> name1 >> name2)) {
+    throw std::runtime_error("Invalid input");
+  }
+  try {
+    datasets.at(new_name);
+    throw std::runtime_error("Dataset exists");
+  } catch (const std::out_of_range &) {
+  }
+  const karpovich::Dataset &ds1 = datasets.at(name1);
+  const karpovich::Dataset &ds2 = datasets.at(name2);
+  karpovich::Dataset new_ds;
+  for (auto it = ds1.cbegin(); it != ds1.cend(); ++it) {
+    auto pair = *it;
+    new_ds.push(pair.first, pair.second);
+  }
+  for (auto it = ds2.cbegin(); it != ds2.cend(); ++it) {
+    auto pair = *it;
+    try {
+      new_ds.at(pair.first);
+    } catch (const std::out_of_range &) {
+      new_ds.push(pair.first, pair.second);
+    }
+  }
+  datasets.push(new_name, std::move(new_ds));
+}
