d60acab0074a81fb70f2d5d399cb2b4525c679cc
voyadger01 <voyadgerkx@gmail.com>
2026-05-07 20:34:27 +0300

    add game types(scenes, items) and serialization hpp files

---
+++ karpovich.dmitriy/F0/gameTypes.hpp
@@ -0,0 +1,32 @@
+#ifndef GAME_TYPES_HPP
+#define GAME_TYPES_HPP
+
+namespace karpovich
+{
+  struct scene_t
+  {
+  };
+
+  struct scene_link_t
+  {
+  };
+
+  struct scene_object_t
+  {
+  };
+
+  struct project_t
+  {
+  };
+
+  struct save_state_t
+  {
+  };
+
+  struct item_template_t
+  {
+  };
+
+}
+
+#endif
---
+++ karpovich.dmitriy/F0/serialization.hpp
@@ -0,0 +1,27 @@
+#ifndef SERIALIZATION_HPP
+#define SERIALIZATION_HPP
+
+#include <gameTypes.hpp>
+#include <iosfwd>
+
+namespace karpovich
+{
+  void serializeItem(const item_template_t &item, std::ostream &out);
+  item_template_t deserializeItem(std::istream &in);
+
+  void serializeLink(const scene_link_t &link, std::ostream &out);
+  scene_link_t deserializeLink(std::istream &in);
+
+  void serializeObject(const scene_object_t &obj, std::ostream &out);
+  scene_object_t deserializeObject(std::istream &in);
+
+  void serializeScene(const scene_t &scene, std::ostream &out);
+  scene_t deserializeScene(std::istream &in);
+
+  void serializeProject(const project_t &project, std::ostream &out);
+  project_t deserializeProject(std::istream &in);
+
+  void serializeSave(const save_state_t &state, std::ostream &out);
+  save_state_t deserializeSave(std::istream &in);
+}
+#endif
