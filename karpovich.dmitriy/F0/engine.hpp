e3bf7130e778bbc044370de8e81130f3d363280b
voyadger01 <voyadgerkx@gmail.com>
2026-05-07 21:10:36 +0300

    add engine hpp

---
+++ karpovich.dmitriy/F0/engine.hpp
@@ -0,0 +1,47 @@
+#ifndef ENGINE_HPP
+#define ENGINE_HPP
+
+#include <string>
+#include "../common/Vector.hpp"
+
+namespace karpovich
+{
+  class Engine
+  {
+  public:
+    Engine();
+    void processCommand(const std::string &line);
+
+  private:
+    void cmdCreateItem(const Vector< std::string > &args);
+    void cmdRemoveItem(const Vector< std::string > &args);
+    void cmdAddAssetDb(const Vector< std::string > &args);
+    void cmdRemoveAssetDb(const Vector< std::string > &args);
+    void cmdShowDb(const Vector< std::string > &args);
+
+    void cmdCreateGame(const Vector< std::string > &args);
+    void cmdLoadProject(const Vector< std::string > &args);
+    void cmdSaveProject(const Vector< std::string > &args);
+    void cmdValidate(const Vector< std::string > &args);
+
+    void cmdCreateScene(const Vector< std::string > &args);
+    void cmdRemoveScene(const Vector< std::string > &args);
+    void cmdLinkScene(const Vector< std::string > &args);
+    void cmdUnlinkScene(const Vector< std::string > &args);
+    void cmdAddObject(const Vector< std::string > &args);
+    void cmdRemoveObject(const Vector< std::string > &args);
+    void cmdSetInteract(const Vector< std::string > &args);
+
+    void cmdStart(const Vector< std::string > &args);
+    void cmdLook(const Vector< std::string > &args);
+    void cmdInteract(const Vector< std::string > &args);
+    void cmdChoice(const Vector< std::string > &args);
+    void cmdShowInv(const Vector< std::string > &args);
+    void cmdSaveGame(const Vector< std::string > &args);
+    void cmdLoadGame(const Vector< std::string > &args);
+    void cmdMode(const Vector< std::string > &args);
+    void cmdFaq(const Vector< std::string > &args);
+  };
+}
+
+#endif
