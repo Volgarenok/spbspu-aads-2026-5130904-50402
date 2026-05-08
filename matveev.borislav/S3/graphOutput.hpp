0ce989825ae5f0977d1ecd835de665a3e24967a7
L1sa620 <lisevbom@gmail.com>
2026-05-08 18:48:27 +0300

    Add graph sorted name

---
+++ matveev.borislav/S3/graphOutput.hpp
@@ -0,0 +1,39 @@
+#ifndef MATVEEV_GRAPH_ALGORITHMS_HPP
+#define MATVEEV_GRAPH_ALGORITHMS_HPP
+
+#include "graphCollection.hpp"
+
+#include "../common/list.hpp"
+
+#include <string>
+
+namespace matveev
+{
+inline void insertSorted(List< std::string >& list, const std::string& value)
+{
+  LIter< std::string > prev = list.beforeBegin();
+  LIter< std::string > it = list.begin();
+
+  while (it != list.end() && *it < value)
+  {
+    ++prev;
+    ++it;
+  }
+
+  list.insertAfter(prev, value);
+}
+
+inline List< std::string > collectGraphNames(const GraphCollection& graphs)
+{
+  List< std::string > result;
+
+  for (auto it = graphs.graphs().cbegin(); it != graphs.graphs().cend(); ++it)
+  {
+    insertSorted(result, it->key);
+  }
+
+  return result;
+}
+}
+
+#endif
--- matveev.borislav/S3/main.cpp
+++ matveev.borislav/S3/main.cpp
@@ -1,18 +1,22 @@
-#include "graphCollection.hpp"
+#include "graphOutput.hpp"
 
 #include <iostream>
+#include <string>
 
 int main()
 {
   matveev::GraphCollection graphs;
   matveev::Graph graph;
 
-  graph.addVertex("a");
-  graphs.addGraph("gr1", graph);
+  graphs.addGraph("ccc", graph);
+  graphs.addGraph("aaa", graph);
+  graphs.addGraph("bbb", graph);
 
-  for (auto it = graphs.graphs().cbegin(); it != graphs.graphs().cend(); ++it)
+  matveev::List< std::string > names = matveev::collectGraphNames(graphs);
+
+  for (matveev::LIter< std::string > it = names.begin(); it != names.end(); ++it)
   {
-    std::cout << it->key << '\n';
+    std::cout << *it << '\n';
   }
 
   return 0;
