3aeaf6add89b978305dca189c3db2d3a2627625f
L1sa620 <lisevbom@gmail.com>
2026-05-09 20:19:17 +0300

    Add handle cut

--- matveev.borislav/S3/main.cpp
+++ matveev.borislav/S3/main.cpp
@@ -7,17 +7,19 @@ int main()
   matveev::GraphCollection graphs;
   matveev::Graph graph;
 
+  graph.bind("a", "b", 10);
+  graph.bind("a", "b", 20);
   graphs.addGraph("gr1", graph);
 
   matveev::List< std::string > tokens;
 
-  tokens = matveev::splitLine("bind gr1 a b 10");
+  tokens = matveev::splitLine("cut gr1 a b 20");
   matveev::executeCommand(std::cout, graphs, tokens);
 
   tokens = matveev::splitLine("outbound gr1 a");
   matveev::executeCommand(std::cout, graphs, tokens);
 
-  tokens = matveev::splitLine("bind gr1 a b x");
+  tokens = matveev::splitLine("cut gr1 a b 30");
   matveev::executeCommand(std::cout, graphs, tokens);
 
   return 0;
