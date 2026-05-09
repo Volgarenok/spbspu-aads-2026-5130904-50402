cbced548e32fcc47dbf30bebb15d4dd8b67e400e
L1sa620 <lisevbom@gmail.com>
2026-05-09 22:46:57 +0300

    Add main program

--- matveev.borislav/S3/main.cpp
+++ matveev.borislav/S3/main.cpp
@@ -1,18 +1,46 @@
-#include "graphReader.hpp"
 #include "commandHandler.hpp"
+#include "graphReader.hpp"
+#include "parserUtils.hpp"
 
 #include <fstream>
 #include <iostream>
+#include <string>
 
-int main()
+int main(int argc, char* argv[])
 {
-  std::ifstream input("test.txt");
+  if (argc != 2)
+  {
+    std::cerr << "<INVALID COMMAND>\n";
+    return 1;
+  }
+
+  std::ifstream input(argv[1]);
+
+  if (!input)
+  {
+    std::cerr << "<INVALID COMMAND>\n";
+    return 1;
+  }
+
   matveev::GraphCollection graphs;
 
-  matveev::readGraphs(input, graphs);
+  try
+  {
+    matveev::readGraphs(input, graphs);
+  }
+  catch (const std::exception&)
+  {
+    std::cerr << "<INVALID COMMAND>\n";
+    return 1;
+  }
 
-  matveev::List< std::string > tokens = matveev::splitLine("outbound gr1 c");
-  matveev::executeCommand(std::cout, graphs, tokens);
+  std::string line;
+
+  while (std::getline(std::cin, line))
+  {
+    matveev::List< std::string > tokens = matveev::splitLine(line);
+    matveev::executeCommand(std::cout, graphs, tokens);
+  }
 
   return 0;
 }
