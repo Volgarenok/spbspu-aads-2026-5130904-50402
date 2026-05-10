ce035af57fa936c1559b7105e2e3a78c6c870819
L1sa620 <lisevbom@gmail.com>
2026-05-10 14:41:29 +0300

    Add overflow tests

--- matveev.borislav/S3/test-hash.cpp
+++ matveev.borislav/S3/test-hash.cpp
@@ -81,3 +81,61 @@ BOOST_AUTO_TEST_CASE(clear_removes_all_values)
   BOOST_TEST(!table.has(1));
   BOOST_TEST(!table.has(2));
 }
+
+namespace
+{
+struct SameHash
+{
+  size_t operator()(int) const
+  {
+    return 0;
+  }
+};
+}
+
+BOOST_AUTO_TEST_CASE(uses_overflow_bucket)
+{
+  matveev::HashTable< int, std::string, SameHash, IntEqual > table(1, 2);
+
+  table.add(1, "one");
+  table.add(2, "two");
+  table.add(3, "three");
+
+  BOOST_TEST(table.at(1) == "one");
+  BOOST_TEST(table.at(2) == "two");
+  BOOST_TEST(table.at(3) == "three");
+
+  BOOST_TEST(table.size() == 3);
+}
+
+BOOST_AUTO_TEST_CASE(overflow_bucket_limit_throws)
+{
+  matveev::HashTable< int, std::string, SameHash, IntEqual > table(1, 2);
+
+  table.add(1, "one");
+  table.add(2, "two");
+  table.add(3, "three");
+  table.add(4, "four");
+
+  BOOST_CHECK_THROW(table.add(5, "five"), std::overflow_error);
+}
+
+BOOST_AUTO_TEST_CASE(rehash_keeps_values)
+{
+  matveev::HashTable< int, std::string, SameHash, IntEqual > table(1, 2);
+
+  table.add(1, "one");
+  table.add(2, "two");
+  table.add(3, "three");
+
+  table.rehash(2, 3);
+
+  BOOST_TEST(table.bucketCount() == 2);
+  BOOST_TEST(table.bucketCapacity() == 3);
+
+  BOOST_TEST(table.at(1) == "one");
+  BOOST_TEST(table.at(2) == "two");
+  BOOST_TEST(table.at(3) == "three");
+
+  BOOST_TEST(table.size() == 3);
+}
