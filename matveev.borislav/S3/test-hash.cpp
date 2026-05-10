8c3f37d4056ba36ae3656faa03004bcb779ab074
L1sa620 <lisevbom@gmail.com>
2026-05-10 15:49:28 +0300

    Add more boost tests

--- matveev.borislav/S3/test-hash.cpp
+++ matveev.borislav/S3/test-hash.cpp
@@ -139,3 +139,57 @@ BOOST_AUTO_TEST_CASE(rehash_keeps_values)
 
   BOOST_TEST(table.size() == 3);
 }
+
+BOOST_AUTO_TEST_CASE(copy_constructor_copies_values)
+{
+  matveev::HashTable< int, std::string, SameHash, IntEqual > table(1, 2);
+
+  table.add(1, "one");
+  table.add(2, "two");
+  table.add(3, "three");
+
+  matveev::HashTable< int, std::string, SameHash, IntEqual > copy(table);
+
+  BOOST_TEST(copy.size() == 3);
+
+  BOOST_TEST(copy.at(1) == "one");
+  BOOST_TEST(copy.at(2) == "two");
+  BOOST_TEST(copy.at(3) == "three");
+}
+
+BOOST_AUTO_TEST_CASE(assignment_operator_copies_values)
+{
+  matveev::HashTable< int, std::string, SameHash, IntEqual > source(1, 2);
+
+  source.add(1, "one");
+  source.add(2, "two");
+  source.add(3, "three");
+
+  matveev::HashTable< int, std::string, SameHash, IntEqual > target(2, 2);
+
+  target = source;
+
+  BOOST_TEST(target.size() == 3);
+
+  BOOST_TEST(target.at(1) == "one");
+  BOOST_TEST(target.at(2) == "two");
+  BOOST_TEST(target.at(3) == "three");
+}
+
+BOOST_AUTO_TEST_CASE(iterator_visits_only_occupied_items)
+{
+  matveev::HashTable< int, std::string, SameHash, IntEqual > table(1, 2);
+
+  table.add(1, "one");
+  table.add(2, "two");
+  table.add(3, "three");
+
+  size_t count = 0;
+
+  for (auto it = table.begin(); it != table.end(); ++it)
+  {
+    ++count;
+  }
+
+  BOOST_TEST(count == 3);
+}
