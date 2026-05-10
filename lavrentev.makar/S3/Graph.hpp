00267488b149b6192bd8e14f5fcfc6901f7ba454
lavrentev.makar <makar.lavrentev@yandex.ru>
2026-05-10 15:10:29 +0300

    Added 'vertexes' method to graph

--- lavrentev.makar/S3/Graph.hpp
+++ lavrentev.makar/S3/Graph.hpp
@@ -2,20 +2,23 @@
 #define GRAPH_HPP
 #include "HashTable.hpp"
 #include "List.hpp"
+#include <stdexcept>
 
 namespace lavrentev{
   struct Graph {
     public:
-
+      void vertexes(List<std::pair<std::string, Graph>> grs, std::string name);
     private:
       HashTable< std::pair<std::string, std::string>,
         List< size_t >,
         Siphash< std::string >,
         std::equal_to<std::pair<std::string, std::string>>
       > gr;
+      List<std::string> vrtxs;
   };
 
   void graphs(List<std::pair<std::string, Graph>> grs);
+  
 }
 
 inline void lavrentev::graphs(List<std::pair<std::string, Graph>> grs)
@@ -26,5 +29,24 @@ inline void lavrentev::graphs(List<std::pair<std::string, Graph>> grs)
     std::cout << (*it).first << "\n";
   }
 }
+
+inline void lavrentev::Graph::vertexes(List<std::pair<std::string, Graph>> grs, std::string name)
+{
+  LCIter<std::pair<std::string, Graph>> it;
+  for (it = grs.cbegin(); it != grs.cend(); ++it)
+  {
+    if ((*it).first == name)
+    {
+      LCIter<std::string> vrtxIt;
+      for (vrtxIt = (*it).second.vrtxs.cbegin(); vrtxIt != (*it).second.vrtxs.cend(); ++vrtxIt)
+      {
+        std::cout << (*vrtxIt) << "\n";
+      }
+    }
+  }
+  throw std::invalid_argument("<INVALID COMMAND>");
+}
+
+
 
 #endif
