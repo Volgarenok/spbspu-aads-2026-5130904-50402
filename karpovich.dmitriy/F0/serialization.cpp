9b6b08659c628f0970b9dc2ce9e1398eb1c45dc7
voyadger01 <voyadgerkx@gmail.com>
2026-05-12 21:25:15 +0300

    i want sstream 3: add deserialize save proj

--- karpovich.dmitriy/F0/serialization.cpp
+++ karpovich.dmitriy/F0/serialization.cpp
@@ -171,6 +171,67 @@ void karpovich::serializeProject(const project_t &project, std::ostream &out)
   out << "END_PROJECT\n";
 }
 
+karpovich::project_t karpovich::deserializeProject(std::istream &in)
+{
+  std::string line = readLine(in);
+  checkPrefix(line, "PROJECT ");
+  size_t pos = 8;
+  project_t res;
+  res.title_ = extractQuoted(line, pos);
+  res.start_scene_id_ = extractToken(line, pos);
+
+  line = readLine(in);
+  while (line != "END_PROJECT") {
+    if (line.size() >= 6 && line.substr(0, 6) == "SCENE ") {
+      scene_t scene;
+      size_t scene_pos = 6;
+      scene.id_ = extractToken(line, scene_pos);
+      scene.description_ = extractQuoted(line, scene_pos);
+
+      line = readLine(in);
+      while (line != "END_LINKS") {
+        if (line.size() >= 5 && line.substr(0, 5) == "LINK ") {
+          size_t link_pos = 5;
+          scene_link_t link;
+          link.target_ = extractToken(line, link_pos);
+          link.description_ = extractQuoted(line, link_pos);
+          link.condition_ = extractToken(line, link_pos);
+          scene.links_.pushBack(link);
+        } else {
+          throw std::runtime_error("Expected LINK or END_LINKS");
+        }
+        line = readLine(in);
+      }
+
+      line = readLine(in);
+      while (line != "END_OBJECTS") {
+        if (line.size() >= 4 && line.substr(0, 4) == "OBJ ") {
+          size_t obj_pos = 4;
+          scene_object_t obj;
+          obj.key_ = extractToken(line, obj_pos);
+          obj.action_ = extractQuoted(line, obj_pos);
+          obj.gives_ = extractToken(line, obj_pos);
+          obj.requires_ = extractToken(line, obj_pos);
+          scene.objects_.pushBack(obj);
+        } else {
+          throw std::runtime_error("Expected OBJ or END_OBJECTS");
+        }
+        line = readLine(in);
+      }
+
+      line = readLine(in);
+      if (line != "END_SCENE") {
+        throw std::runtime_error("Expected END_SCENE");
+      }
+      res.scenes_.add(scene.id_, std::move(scene));
+    } else {
+      throw std::runtime_error("Expected SCENE or END_PROJECT");
+    }
+    line = readLine(in);
+  }
+  return res;
+}
+
 void karpovich::serializeSave(const save_state_t &state, std::ostream &out)
 {
   out << "SAVE " << state.current_scene_id_ << "\n";
@@ -179,4 +240,31 @@ void karpovich::serializeSave(const save_state_t &state, std::ostream &out)
     out << "INV " << (*it).first << " " << (*it).second << "\n";
   }
   out << "END_SAVE\n";
+}
+
+karpovich::save_state_t karpovich::deserializeSave(std::istream &in)
+{
+  std::string line = readLine(in);
+  checkPrefix(line, "SAVE ");
+  size_t pos = 5;
+  save_state_t res;
+  res.current_scene_id_ = extractToken(line, pos);
+
+  line = readLine(in);
+  while (line != "END_SAVE") {
+    if (line.size() >= 4 && line.substr(0, 4) == "INV ") {
+      size_t inv_pos = 4;
+      std::string key = extractToken(line, inv_pos);
+      std::string count_str = extractToken(line, inv_pos);
+      int count = 0;
+      for (size_t i = 0; i < count_str.size(); ++i) {
+        count = count * 10 + (count_str[i] - '0');
+      }
+      res.inventory_.add(key, count);
+    } else {
+      throw std::runtime_error("Expected INV or END_SAVE");
+    }
+    line = readLine(in);
+  }
+  return res;
 }
