1beed83c757729909e0eaeb150b619b4516f9329
lavrentev.makar <makar.lavrentev@yandex.ru>
2026-05-14 18:11:03 +0300

    Fixed CG in tests S1

--- lavrentev.makar/S1/test-List.cpp
+++ lavrentev.makar/S1/test-List.cpp
@@ -1,101 +1,131 @@
 #define BOOST_TEST_MODULE ListTest
+
 #include <boost/test/included/unit_test.hpp>
 #include "List.hpp"
 
 BOOST_AUTO_TEST_CASE(clear_test)
 {
-  lavrentev::List<size_t> k{};
-  lavrentev::LIter<size_t> it;
-  it = k.insert(it, 3);
-  it = k.insert(it, 4);
-  k.clear();
-  BOOST_CHECK(k.cbegin() == lavrentev::LCIter<size_t>());
+  lavrentev::List< size_t > list{};
+  lavrentev::LIter< size_t > iter;
+
+  iter = list.insert(iter, 3);
+  iter = list.insert(iter, 4);
+
+  list.clear();
+
+  lavrentev::LCIter< size_t > empty_iter;
+  BOOST_CHECK(list.cbegin() == empty_iter);
 }
 
 BOOST_AUTO_TEST_CASE(insert_test)
 {
-  lavrentev::List<size_t> k{};
-  lavrentev::LIter<size_t> it;
-  it = k.insert(it, 1);
-  BOOST_TEST(k.front() == 1);
+  lavrentev::List< size_t > list{};
+  lavrentev::LIter< size_t > iter;
+
+  iter = list.insert(iter, 1);
+
+  BOOST_TEST(list.front() == 1);
 }
 
-BOOST_AUTO_TEST_CASE(popFront_test)
+BOOST_AUTO_TEST_CASE(pop_front_test)
 {
-  lavrentev::List<size_t> k{};
-  BOOST_CHECK(k.popFront() == lavrentev::LIter<size_t>{});
-  lavrentev::LIter<size_t> it;
-  it = k.insert(it, 1);
-  it = k.insert(it, 2);
-  BOOST_CHECK(k.popFront() == it);
-  BOOST_CHECK(k.popFront() == lavrentev::LIter<size_t>{});
+  lavrentev::List< size_t > list{};
+  lavrentev::LIter< size_t > empty_iter;
+
+  BOOST_CHECK(list.popFront() == empty_iter);
+
+  lavrentev::LIter< size_t > iter;
+  iter = list.insert(iter, 1);
+  iter = list.insert(iter, 2);
+
+  BOOST_CHECK(list.popFront() == iter);
+  BOOST_CHECK(list.popFront() == empty_iter);
 }
 
 BOOST_AUTO_TEST_CASE(begin_test)
 {
-  lavrentev::List<size_t> k{};
-  BOOST_CHECK(k.begin() == lavrentev::LIter<size_t>());
+  lavrentev::List< size_t > list{};
+  lavrentev::LIter< size_t > empty_iter;
 
-  lavrentev::LIter<size_t> it;
-  it = k.insert(it, 3);
-  BOOST_CHECK(k.begin() == it);
-  BOOST_TEST(*k.begin() == 3);
+  BOOST_CHECK(list.begin() == empty_iter);
+
+  lavrentev::LIter< size_t > iter;
+  iter = list.insert(iter, 3);
+
+  BOOST_CHECK(list.begin() == iter);
+  BOOST_TEST(*list.begin() == 3);
 }
 
 BOOST_AUTO_TEST_CASE(cbegin_test)
 {
-  lavrentev::List<size_t> k{};
-  BOOST_CHECK(k.cbegin() == lavrentev::LCIter<size_t>());
+  lavrentev::List< size_t > list{};
+  lavrentev::LCIter< size_t > empty_iter;
 
-  lavrentev::LIter<size_t> it;
-  it = k.insert(it, 3);
-  lavrentev::LCIter<size_t> cit = k.cbegin();
-  BOOST_CHECK(cit != lavrentev::LCIter<size_t>());
-  BOOST_TEST(*cit == 3);
+  BOOST_CHECK(list.cbegin() == empty_iter);
+
+  lavrentev::LIter< size_t > iter;
+  iter = list.insert(iter, 3);
+
+  lavrentev::LCIter< size_t > citer = list.cbegin();
+
+  BOOST_CHECK(citer != empty_iter);
+  BOOST_TEST(*citer == 3);
 }
 
 BOOST_AUTO_TEST_CASE(end_test)
 {
-  lavrentev::List<size_t> k{};
-  BOOST_CHECK(k.end() == lavrentev::LIter<size_t>());
+  lavrentev::List< size_t > list{};
+  lavrentev::LIter< size_t > empty_iter;
+
+  BOOST_CHECK(list.end() == empty_iter);
 }
 
 BOOST_AUTO_TEST_CASE(cend_test)
 {
-  lavrentev::List<size_t> k{};
-  BOOST_CHECK(k.cend() == lavrentev::LCIter<size_t>());
+  lavrentev::List< size_t > list{};
+  lavrentev::LCIter< size_t > empty_iter;
+
+  BOOST_CHECK(list.cend() == empty_iter);
 }
 
 BOOST_AUTO_TEST_CASE(front_test)
 {
-  lavrentev::List<size_t> k{};
-  BOOST_CHECK_THROW(k.front(), std::out_of_range);
+  lavrentev::List< size_t > list{};
 
-  lavrentev::LIter<size_t> it;
-  it = k.insert(it, 3);
-  BOOST_TEST(k.front() == 3);
+  BOOST_CHECK_THROW(list.front(), std::out_of_range);
 
-  const lavrentev::List<size_t> &ck = k;
-  BOOST_TEST(ck.front() == 3);
+  lavrentev::LIter< size_t > iter;
+  iter = list.insert(iter, 3);
+
+  BOOST_TEST(list.front() == 3);
+
+  const lavrentev::List< size_t >& const_list = list;
+  BOOST_TEST(const_list.front() == 3);
 }
 
 BOOST_AUTO_TEST_CASE(empty_test)
 {
-  lavrentev::List<size_t> k{};
-  BOOST_TEST(k.empty());
+  lavrentev::List< size_t > list{};
 
-  lavrentev::LIter<size_t> it;
-  it = k.insert(it, 3);
-  BOOST_TEST(!k.empty());
+  BOOST_TEST(list.empty());
+
+  lavrentev::LIter< size_t > iter;
+  iter = list.insert(iter, 3);
+
+  BOOST_TEST(!list.empty());
 }
 
 BOOST_AUTO_TEST_CASE(remove_test)
 {
-  lavrentev::List<size_t> k{};
-  BOOST_CHECK(k.remove(1) == lavrentev::LIter<size_t>{});
-  lavrentev::LIter<size_t> it;
-  it = k.insert(it, 1);
-  it = k.insert(it, 2);
-  BOOST_CHECK(k.remove(1) == it);
-  BOOST_CHECK(k.remove(2) == lavrentev::LIter<size_t>{});
+  lavrentev::List< size_t > list{};
+  lavrentev::LIter< size_t > empty_iter;
+
+  BOOST_CHECK(list.remove(1) == empty_iter);
+
+  lavrentev::LIter< size_t > iter;
+  iter = list.insert(iter, 1);
+  iter = list.insert(iter, 2);
+
+  BOOST_CHECK(list.remove(1) == iter);
+  BOOST_CHECK(list.remove(2) == empty_iter);
 }
