9fe3f27a3e32b56a48a06980539d04decea8eb18
Kayv1e <128278935+Kayv1e@users.noreply.github.com>
2026-05-15 11:16:41 +0300

    Added basic main implementation, queue and stack headers and test-main

--- novikov.ivan/S2/main.cpp
+++ novikov.ivan/S2/main.cpp
@@ -1,2 +1,41 @@
-int main()
-{}
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "stack.hpp"
+
+namespace novikov
+{
+    Stack< std::string > input(std::istream& in);
+    int eval(std::string line);
+}
+
+int main(int argc, char** argv)
+{
+    novikov::Stack< std::string > expressions;
+    if (argc == 1)
+    {
+        expressions = novikov::input(std::cin);
+    }
+    else if (argc == 2)
+    {
+        std::ifstream in(argv[1]);
+        expressions = novikov::input(in);
+    }
+    else
+    {
+        std::cerr << "Invalid arguments\n";
+        return 1;
+    }
+
+    while (!expressions.empty())
+    {
+        int res = novikov::eval(expressions.top());
+        std::cout << res;
+        expressions.pop();
+        if (!expressions.empty())
+        {
+            std::cout << ' ';
+        }
+    }
+    std::cout << '\n';
+}
