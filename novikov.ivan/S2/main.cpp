e6f2f051743e050cceda9cb856bbe79a4830fe94
Kayv1e <128278935+Kayv1e@users.noreply.github.com>
2026-05-15 11:58:32 +0300

    Added getPriority, isOperation and infixToPostfix functions

--- novikov.ivan/S2/main.cpp
+++ novikov.ivan/S2/main.cpp
@@ -8,9 +8,11 @@
 namespace novikov
 {
     Stack< std::string > input(std::istream& in);
-    std::string infixToPostfix(const std::string& infix);
     Queue< std::string > split(const std::string& line);
     long long eval(std::string line);
+    int getPriority(const std::string& op);
+    bool isOperation(const std::string& s);
+    Queue< std::string > infixToPostfix(novikov::Queue< std::string > infix);
 }
 
 int main(int argc, char** argv)
@@ -71,16 +73,16 @@ novikov::Stack< std::string > novikov::input(std::istream& in)
 
 long long novikov::eval(std::string line)
 {
-    std::string postfix = infixToPostfix(line);
-    Queue< std::string > exp = split(line);
+    Queue< std::string > infix = split(line);
+    Queue< std::string > postfix = infixToPostfix(infix);
     Stack< long long > results;
     results.push(0);
     long long operand1 = 0, operand2 = 0;
     int status = 0;
 
-    while (!exp.empty())
+    while (!postfix.empty())
     {
-        std::string val = exp.front();
+        std::string val = postfix.front();
         if (val == "+")
         {
             if (status == 2)
@@ -214,7 +216,101 @@ long long novikov::eval(std::string line)
                 status = 2;
             }
         }
-        exp.pop();
+        postfix.pop();
     }
     return results.top();
+}
+
+int novikov::getPriority(const std::string& op)
+{
+    if (op == "|")
+    {
+        return 3;
+    }
+    if (op == "*" || op == "/" || op == "%")
+    {
+        return 2;
+    }
+    if (op == "+" || op == "-")
+    {
+        return 1;
+    }
+    return 0;
+}
+
+bool novikov::isOperation(const std::string& s)
+{
+    bool res = s == "|";
+    res = res || s == "*" || s == "/" || s == "%";
+    res = res || s == "+" || s == "-";
+    return res;
+}
+
+novikov::Queue< std::string > infixToPostfix(novikov::Queue< std::string > infix)
+{
+    novikov::Queue< std::string > postfix;
+    novikov::Stack< std::string > stack;
+    while (!postfix.empty())
+    {
+        std::string val = infix.front();
+        if (val == "(")
+        {
+            stack.push(val);
+        }
+        else if (val == ")")
+        {
+            std::string op = stack.top();
+            while (novikov::isOperation(op))
+            {
+                postfix.push(op);
+                stack.pop();
+                op = stack.top();
+            }
+            if (stack.top() == "(")
+            {
+                stack.pop();
+            }
+        }
+        else if (novikov::isOperation(val))
+        {
+            std::string op = stack.top();
+            while (novikov::getPriority(op) <= novikov::getPriority(val))
+            {
+                postfix.push(op);
+                stack.pop();
+                op = stack.top();
+            }
+            stack.push(val);
+        }
+        else
+        {
+            postfix.push(val);
+        }
+        infix.pop();
+    }
+    while (!stack.empty())
+    {
+        postfix.push(stack.top());
+        stack.pop();
+    }
+    return postfix;
+}
+
+novikov::Queue< std::string > split(const std::string& line)
+{
+    novikov::Queue< std::string > res;
+    std::string curr;
+    for (size_t i = 0; i < line.length(); ++i)
+    {
+        if (line[i] == ' ')
+        {
+            res.push(curr);
+            curr.clear();
+        }
+        else
+        {
+            curr += line[i];
+        }
+    }
+    return res;
 }
