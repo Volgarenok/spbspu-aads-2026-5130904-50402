986215444cb97b1add57f1b772e43bf025b562a5
Kayv1e <128278935+Kayv1e@users.noreply.github.com>
2026-05-15 11:28:39 +0300

    Added input and eval functions

--- novikov.ivan/S2/main.cpp
+++ novikov.ivan/S2/main.cpp
@@ -1,12 +1,16 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <stdexcept>
 #include "stack.hpp"
+#include "queue.hpp"
 
 namespace novikov
 {
     Stack< std::string > input(std::istream& in);
-    int eval(std::string line);
+    std::string infixToPostfix(const std::string& infix);
+    Queue< std::string > split(const std::string& line);
+    long long eval(std::string line);
 }
 
 int main(int argc, char** argv)
@@ -29,7 +33,21 @@ int main(int argc, char** argv)
 
     while (!expressions.empty())
     {
-        int res = novikov::eval(expressions.top());
+        long long res;
+        try
+        {
+            res = novikov::eval(expressions.top());
+        }
+        catch (const std::invalid_argument&)
+        {
+            std::cerr << "Invalid arguments\n";
+            return 1;
+        }
+        catch (const std::out_of_range&)
+        {
+            std::cerr << "Out of range\n";
+            return 2;
+        }
         std::cout << res;
         expressions.pop();
         if (!expressions.empty())
@@ -38,4 +56,165 @@ int main(int argc, char** argv)
         }
     }
     std::cout << '\n';
+}
+
+novikov::Stack< std::string > novikov::input(std::istream& in)
+{
+    Stack< std::string > res;
+    std::string exp;
+    while (std::getline(in, exp))
+    {
+        res.push(exp);
+    }
+    return res;
+}
+
+long long novikov::eval(std::string line)
+{
+    std::string postfix = infixToPostfix(line);
+    Queue< std::string > exp = split(line);
+    Stack< long long > results;
+    results.push(0);
+    long long operand1 = 0, operand2 = 0;
+    int status = 0;
+
+    while (!exp.empty())
+    {
+        std::string val = exp.front();
+        if (val == "+")
+        {
+            if (status == 2)
+            {
+                results.push(operand1 + operand2);
+            }
+            else if (status == 1)
+            {
+                results.top() += operand1;
+            }
+            else if (status == 0)
+            {
+                long long op1 = results.top();
+                results.pop();
+                long long op2 = results.top();
+                results.pop();
+                results.top() = op1 + op2;
+            }
+            status = 0;
+        }
+        else if (val == "-")
+        {
+            if (status == 2)
+            {
+                results.push(operand1 - operand2);
+            }
+            else if (status == 1)
+            {
+                results.top() -= operand1;
+            }
+            else if (status == 0)
+            {
+                long long op1 = results.top();
+                results.pop();
+                long long op2 = results.top();
+                results.pop();
+                results.top() = op1 - op2;
+            }
+            status = 0;
+        }
+        else if (val == "*")
+        {
+            if (status == 2)
+            {
+                results.push(operand1 * operand2);
+            }
+            else if (status == 1)
+            {
+                results.top() *= operand1;
+            }
+            else if (status == 0)
+            {
+                long long op1 = results.top();
+                results.pop();
+                long long op2 = results.top();
+                results.pop();
+                results.top() = op1 * op2;
+            }
+            status = 0;
+        }
+        else if (val == "/")
+        {
+            if (status == 2)
+            {
+                results.push(operand1 / operand2);
+            }
+            else if (status == 1)
+            {
+                results.top() /= operand1;
+            }
+            else if (status == 0)
+            {
+                long long op1 = results.top();
+                results.pop();
+                long long op2 = results.top();
+                results.pop();
+                results.top() = op1 / op2;
+            }
+            status = 0;
+        }
+        else if (val == "%")
+        {
+            if (status == 2)
+            {
+                results.push(operand1 % operand2);
+            }
+            else if (status == 1)
+            {
+                results.top() %= operand1;
+            }
+            else if (status == 0)
+            {
+                long long op1 = results.top();
+                results.pop();
+                long long op2 = results.top();
+                results.pop();
+                results.top() = op1 % op2;
+            }
+            status = 0;
+        }
+        else if (val == "|")
+        {
+            if (status == 2)
+            {
+                results.push(operand1 | operand2);
+            }
+            else if (status == 1)
+            {
+                results.top() |= operand1;
+            }
+            else if (status == 0)
+            {
+                long long op1 = results.top();
+                results.pop();
+                long long op2 = results.top();
+                results.pop();
+                results.top() = op1 | op2;
+            }
+            status = 0;
+        }
+        else
+        {
+            if (status == 0)
+            {
+                operand1 = std::stoll(val);
+                status = 1;
+            }
+            else if (status == 1)
+            {
+                operand2 = std::stoll(val);
+                status = 2;
+            }
+        }
+        exp.pop();
+    }
+    return results.top();
 }
