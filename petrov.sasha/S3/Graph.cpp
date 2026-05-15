37c526959fce99d1946a2bf1a1ffa3b5ff790b39
sp0146001 <sp0146001@gmail.com>
2026-05-16 00:39:37 +0300

    in bound, check size

--- petrov.sasha/S3/Graph.cpp
+++ petrov.sasha/S3/Graph.cpp
@@ -37,7 +37,7 @@ namespace petrov {
     }
   }
 
-  std::vector< std::pair< std::string, std::vector< unsigned int > > > Graph::getOutbound(const std::string& vertex) const
+  std::vector<std::pair<std::string, std::vector<unsigned int>>> Graph::getOutbound(const std::string& vertex) const
   {
     HashTable< std::string, EdgeValue, SHA1Hasher< std::string >, DefaultEqual< std::string > > result(50);
 
@@ -52,6 +52,40 @@ namespace petrov {
           existingWeights.insert(existingWeights.end(), weights.begin(), weights.end());
         } else {
           result.add(to, weights);
+        }
+      }
+    }
+
+    std::vector< std::pair< std::string, EdgeValue > > output;
+    for (auto it = result.begin(); it != result.end(); ++it) {
+      auto pair = *it;
+      EdgeValue sortedWeights = pair.second;
+      std::sort(sortedWeights.begin(), sortedWeights.end());
+      output.push_back({pair.first, sortedWeights});
+    }
+
+    std::sort(output.begin(), output.end(), [](const auto& a, const auto& b) {
+      return a.first < b.first;
+    });
+
+    return output;
+  }
+
+  std::vector< std::pair< std::string, std::vector< unsigned int > > > Graph::getInbound(const std::string& vertex) const
+  {
+    HashTable< std::string, EdgeValue, SHA1Hasher< std::string >, DefaultEqual< std::string > > result(50);
+
+    for (auto it = edges_.cbegin(); it != edges_.cend(); ++it) {
+      auto edge = *it;
+      if (edge.first.second == vertex) {
+        const std::string& from = edge.first.first;
+        const EdgeValue& weights = edge.second;
+
+        if (result.has(from)) {
+          EdgeValue& existingWeights = result.get(from);
+          existingWeights.insert(existingWeights.end(), weights.begin(), weights.end());
+        } else {
+          result.add(from, weights);
         }
       }
     }
