3c5e0c01cace0b949f9c5b82a4d84f483c1725fb
sp0146001 <sp0146001@gmail.com>
2026-05-16 00:04:12 +0300

    and remove this F%cking edge RAHHH

--- petrov.sasha/S3/Graph.cpp
+++ petrov.sasha/S3/Graph.cpp
@@ -20,4 +20,20 @@ namespace petrov {
     }
   }
 
+  void Graph::removeEdge(const std::string& from, const std::string& to, unsigned int weight)
+  {
+    EdgeKey key = {from, to};
+    if (!edges_.has(key)) {
+      throw std::out_of_range("Edge not found");
+    }
+    EdgeValue& weights = edges_.get(key);
+    auto it = std::find(weights.begin(), weights.end(), weight);
+    if (it == weights.end()) {
+      throw std::out_of_range("Weight not found");
+    }
+    weights.erase(it);
+    if (weights.empty()) {
+      edges_.drop(key);
+    }
+  }
 }
