02ad28e616220a0b74d0112490fcbe1e2549af32
sp0146001 <sp0146001@gmail.com>
2026-05-16 00:41:16 +0300

    main file with work commands

--- petrov.sasha/S3/main.cpp
+++ petrov.sasha/S3/main.cpp
@@ -1,1 +1,14 @@
-int main(){}
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <limits>
+#include "Graph.hpp"
+#include "HashTable.hpp"
+#include "SHA1Hash.hpp"
+
+int main(int argc, char* argv[])
+{
+  
+}
