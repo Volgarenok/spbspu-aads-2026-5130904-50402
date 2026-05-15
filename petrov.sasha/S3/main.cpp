5d5def3682033ddf0aa0684cbcdd4db13430f06e
sp0146001 <sp0146001@gmail.com>
2026-05-16 01:55:30 +0300

    all typesd of commends + CG)

--- petrov.sasha/S3/main.cpp
+++ petrov.sasha/S3/main.cpp
@@ -64,9 +64,213 @@ int main(int argc, char* argv[])
     std::string command;
     iss >> command;
     try {
+      if (command == "graphs") {
+        std::vector< std::string > names;
+        for (auto it = graphs.begin(); it != graphs.end(); ++it) {
+          auto pair = *it;
+          names.push_back(pair.first);
+        }
+        std::sort(names.begin(), names.end());
+        for (const auto& name : names) {
+          std::cout << name << "\n";
+        }
+      } else if (command == "vertexes") {
+        std::string graphName;
+        iss >> graphName;
 
+        if (!graphs.has(graphName)) {
+          std::cout << "<INVALID COMMAND>\n";
+          continue;
+        }
+
+        petrov::Graph& graph = graphs.get(graphName);
+        std::vector< std::string > vertices = graph.getVertices();
+        std::sort(vertices.begin(), vertices.end());
+
+        for (const auto& vertex : vertices) {
+          std::cout << vertex << "\n";
+        }
+      } else if (command == "outbound") {
+        std::string graphName;
+        std::string vertex;
+        iss >> graphName >> vertex;
+
+        if (!graphs.has(graphName)) {
+          std::cout << "<INVALID COMMAND>\n";
+          continue;
+        }
+
+        petrov::Graph& graph = graphs.get(graphName);
+        std::vector< std::string > vertices = graph.getVertices();
+
+        if (std::find(vertices.begin(), vertices.end(), vertex) == vertices.end()) {
+          std::cout << "<INVALID COMMAND>\n";
+          continue;
+        }
+
+        auto outbound = graph.getOutbound(vertex);
+        for (const auto& pair : outbound) {
+          std::cout << pair.first;
+          for (unsigned int weight : pair.second) {
+            std::cout << " " << weight;
+          }
+          std::cout << "\n";
+        }
+      } else if (command == "inbound") {
+        std::string graphName;
+        std::string vertex;
+        iss >> graphName >> vertex;
+
+        if (!graphs.has(graphName)) {
+          std::cout << "<INVALID COMMAND>\n";
+          continue;
+        }
+
+        petrov::Graph& graph = graphs.get(graphName);
+        std::vector< std::string > vertices = graph.getVertices();
+
+        if (std::find(vertices.begin(), vertices.end(), vertex) == vertices.end()) {
+          std::cout << "<INVALID COMMAND>\n";
+          continue;
+        }
+
+        auto inbound = graph.getInbound(vertex);
+        for (const auto& pair : inbound) {
+          std::cout << pair.first;
+          for (unsigned int weight : pair.second) {
+            std::cout << " " << weight;
+          }
+          std::cout << "\n";
+        }
+      } else if (command == "bind") {
+        std::string graphName;
+        std::string vertexA;
+        std::string vertexB;
+        unsigned int weight = 0;
+        iss >> graphName >> vertexA >> vertexB >> weight;
+
+        if (!graphs.has(graphName)) {
+          std::cout << "<INVALID COMMAND>\n";
+          continue;
+        }
+
+        petrov::Graph& graph = graphs.get(graphName);
+        graph.addEdge(vertexA, vertexB, weight);
+      } else if (command == "cut") {
+        std::string graphName;
+        std::string vertexA;
+        std::string vertexB;
+        unsigned int weight = 0;
+        iss >> graphName >> vertexA >> vertexB >> weight;
+
+        if (!graphs.has(graphName)) {
+          std::cout << "<INVALID COMMAND>\n";
+          continue;
+        }
+
+        try {
+          petrov::Graph& graph = graphs.get(graphName);
+          graph.removeEdge(vertexA, vertexB, weight);
+        } catch (...) {
+          std::cout << "<INVALID COMMAND>\n";
+        }
+      } else if (command == "create") {
+        std::string graphName;
+        iss >> graphName;
+
+        if (graphs.has(graphName)) {
+          std::cout << "<INVALID COMMAND>\n";
+          continue;
+        }
+
+        petrov::Graph newGraph;
+        graphs.add(graphName, newGraph);
+      } else if (command == "merge") {
+        std::string newGraphName;
+        std::string graph1Name;
+        std::string graph2Name;
+        iss >> newGraphName >> graph1Name >> graph2Name;
+
+        if (graphs.has(newGraphName) || !graphs.has(graph1Name) || !graphs.has(graph2Name)) {
+          std::cout << "<INVALID COMMAND>\n";
+          continue;
+        }
+
+        petrov::Graph newGraph;
+        petrov::Graph& g1 = graphs.get(graph1Name);
+        petrov::Graph& g2 = graphs.get(graph2Name);
+
+        for (auto it = g1.getVertices().begin(); it != g1.getVertices().end(); ++it) {
+          auto outbound = g1.getOutbound(*it);
+          for (const auto& pair : outbound) {
+            for (unsigned int weight : pair.second) {
+              newGraph.addEdge(*it, pair.first, weight);
+            }
+          }
+        }
+
+        for (auto it = g2.getVertices().begin(); it != g2.getVertices().end(); ++it) {
+          auto outbound = g2.getOutbound(*it);
+          for (const auto& pair : outbound) {
+            for (unsigned int weight : pair.second) {
+              newGraph.addEdge(*it, pair.first, weight);
+            }
+          }
+        }
+
+        graphs.add(newGraphName, newGraph);
+      } else if (command == "extract") {
+        std::string newGraphName;
+        std::string oldGraphName;
+        size_t count = 0;
+        iss >> newGraphName >> oldGraphName >> count;
+
+        std::vector< std::string > vertices;
+        for (size_t i = 0; i < count; ++i) {
+          std::string vertex;
+          iss >> vertex;
+          vertices.push_back(vertex);
+        }
+
+        if (graphs.has(newGraphName) || !graphs.has(oldGraphName)) {
+          std::cout << "<INVALID COMMAND>\n";
+          continue;
+        }
+
+        petrov::Graph& oldGraph = graphs.get(oldGraphName);
+        std::vector< std::string > allVertices = oldGraph.getVertices();
+
+        bool valid = true;
+        for (const auto& vertex : vertices) {
+          if (std::find(allVertices.begin(), allVertices.end(), vertex) == allVertices.end()) {
+            valid = false;
+            break;
+          }
+        }
+
+        if (!valid) {
+          std::cout << "<INVALID COMMAND>\n";
+          continue;
+        }
+
+        petrov::Graph newGraph;
+        for (const auto& from : vertices) {
+          auto outbound = oldGraph.getOutbound(from);
+          for (const auto& pair : outbound) {
+            if (std::find(vertices.begin(), vertices.end(), pair.first) != vertices.end()) {
+              for (unsigned int weight : pair.second) {
+                newGraph.addEdge(from, pair.first, weight);
+              }
+            }
+          }
+        }
+
+        graphs.add(newGraphName, newGraph);
+      } else {
+        std::cout << "<INVALID COMMAND>\n";
+      }
     } catch (...) {
-
+      std::cout << "<INVALID COMMAND>\n"
     }
   }
   return 0;
