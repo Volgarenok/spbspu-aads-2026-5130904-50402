87ee0b828bb4fbcad2f1a4a737f13a844bf7f14f
sp0146001 <sp0146001@gmail.com>
2026-05-16 01:28:40 +0300

    finish checking file, next is commands

--- petrov.sasha/S3/main.cpp
+++ petrov.sasha/S3/main.cpp
@@ -20,4 +20,40 @@ int main(int argc, char* argv[])
     std::cerr << "Error opening file\n";
     return 1;
   }
+
+  petrov::HashTable< std::string, petrov::Graph, petrov::SHA1Hasher< std::string >, petrov::DefaultEqual< std::string > > graphs(100);
+
+  std::string line;
+  while (std::getline(file, line)) {
+    if (line.empty()) {
+      continue;
+    }
+
+    std::istringstream iss(line);
+    std::string graphName;
+    size_t edgeCount = 0;
+    iss >> graphName >> edgeCount;
+
+    petrov::Graph graph;
+
+    for (size_t i = 0; i < edgeCount; ++i) {
+      std::string from;
+      std::string to;
+      unsigned int weight = 0;
+
+      if (!(file >> from >> to >> weight)) {
+        std::cerr << "Error reading edge data\n";
+        return 1;
+      }
+
+      graph.addEdge(from, to, weight);
+    }
+
+    file.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+
+    graphs.add(graphName, graph);
+  }
+
+  file.close();
+
 }
