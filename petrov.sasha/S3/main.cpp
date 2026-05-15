d987ebe8022b555517815b708b2121a7ec326d52
sp0146001 <sp0146001@gmail.com>
2026-05-16 00:41:50 +0300

    not enough elements

--- petrov.sasha/S3/main.cpp
+++ petrov.sasha/S3/main.cpp
@@ -10,5 +10,14 @@
 
 int main(int argc, char* argv[])
 {
-  
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <filename>\n";
+    return 1;
+  }
+
+  std::ifstream file(argv[1]);
+  if (!file) {
+    std::cerr << "Error opening file\n";
+    return 1;
+  }
 }
