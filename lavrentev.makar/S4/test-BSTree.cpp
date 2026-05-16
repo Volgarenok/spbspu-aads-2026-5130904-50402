48928e474a86cd032d767ee364ff808b5c49e2fb
lavrentev.makar <makar.lavrentev@yandex.ru>
2026-05-16 22:27:27 +0300

    Added 'push' test

--- lavrentev.makar/S4/test-BSTree.cpp
+++ lavrentev.makar/S4/test-BSTree.cpp
@@ -4,3 +4,10 @@
 #include "BSTree.hpp"
 #include <functional>
 
+BOOST_AUTO_TEST_CASE(push_test)
+{
+  lavrentev::BSTree<std::string, size_t, std::less<size_t>> bst{};
+  bst.push("a", 1);
+  BOOST_TEST(bst["a"] == 1);
+  BOOST_CHECK_THROW(bst.push("a", 1), std::out_of_range);
+}
