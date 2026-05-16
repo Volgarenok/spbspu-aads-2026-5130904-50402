cf36597d070911d7aae791140385a3253b2582e1
s1nhronn <silekatik@yandex.ru>
2026-05-16 21:29:17 +0300

    Add hasher

---
+++ shirokov.arsenii/S3/hasher.hpp
@@ -0,0 +1,36 @@
+#ifndef HASHER_HPP
+#define HASHER_HPP
+#include <boost/hash2/get_integral_result.hpp>
+#include <boost/hash2/hash_append.hpp>
+#include <boost/hash2/sha1.hpp>
+#include <boost/unordered/unordered_flat_map.hpp>
+
+namespace shirokov
+{
+  template < class T >
+  class SHA1
+  {
+  public:
+    SHA1();
+    size_t operator()(const T& v) const;
+
+  private:
+    uint64_t seed_ = 0x8badf00d1badb002;
+    boost::hash2::sha1_160 h_;
+  };
+}
+
+template < class T >
+shirokov::SHA1< T >::SHA1():
+  h_(seed_)
+{}
+
+template < class T >
+size_t shirokov::SHA1< T >::operator()(const T& v) const
+{
+  boost::hash2::sha1_160 h(h_);
+  boost::hash2::hash_append(h, boost::hash2::default_flavor{}, v);
+  return boost::hash2::get_integral_result< size_t >(h);
+}
+
+#endif
