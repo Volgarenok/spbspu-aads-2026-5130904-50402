9888d7f4781221042d668af7590106db4956150f
s1nhronn <silekatik@yandex.ru>
2026-05-17 15:03:13 +0300

    Compile test for insert in empty

--- shirokov.arsenii/S3/test-HashTable.cpp
+++ shirokov.arsenii/S3/test-HashTable.cpp
@@ -1,9 +1,9 @@
-#include <functional>
-#include "hasher.hpp"
 #define BOOST_TEST_MODULE S1
+#include <functional>
 #include <boost/test/included/unit_test.hpp>
 #include <boost/test/unit_test_suite.hpp>
 #include "HashTable.hpp"
+#include "hasher.hpp"
 
 BOOST_AUTO_TEST_CASE(default_constructor)
 {
@@ -11,4 +11,13 @@ BOOST_AUTO_TEST_CASE(default_constructor)
   BOOST_TEST(ht.size() == 0);
   BOOST_TEST(ht.slotsCount() == 0);
   BOOST_TEST(ht.empty());
+}
+
+BOOST_AUTO_TEST_CASE(insert_value_make_non_empty)
+{
+  shirokov::HashTable< int, int, shirokov::SHA1< int >, std::equal_to< int > > ht;
+  ht.insert(0, 1);
+  BOOST_TEST(ht.size() > 0);
+  BOOST_TEST(ht.slotsCount() == 1);
+  BOOST_TEST(!ht.empty());
 }
