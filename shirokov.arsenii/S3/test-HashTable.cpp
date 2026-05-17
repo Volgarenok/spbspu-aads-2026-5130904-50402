6d322414ec93fea36b5309aa832101483aad11a2
s1nhronn <silekatik@yandex.ru>
2026-05-17 15:59:57 +0300

    Edit test

--- shirokov.arsenii/S3/test-HashTable.cpp
+++ shirokov.arsenii/S3/test-HashTable.cpp
@@ -1,4 +1,4 @@
-#define BOOST_TEST_MODULE S1
+#define BOOST_TEST_MODULE S3
 #include <functional>
 #include <boost/test/included/unit_test.hpp>
 #include <boost/test/unit_test_suite.hpp>
@@ -8,7 +8,6 @@
 BOOST_AUTO_TEST_CASE(default_constructor)
 {
   shirokov::HashTable< int, int, shirokov::SHA1< int >, std::equal_to< int > > ht;
-  BOOST_TEST(ht.size() == 0);
   BOOST_TEST(ht.slotsCount() == 0);
   BOOST_TEST(ht.empty());
 }
