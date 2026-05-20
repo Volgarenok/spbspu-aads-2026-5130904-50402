5589de84946f1397a89646af135c9e7495842278
Alastor20 <nik_d.007@mail.ru>
2026-05-20 09:01:41 +0300

    space fix

--- dirko.nikita/S2/queue.hpp
+++ dirko.nikita/S2/queue.hpp
@@ -4,7 +4,7 @@
 
 namespace dirko
 {
-  template < class T >
+  template< class T >
   class Queue
   {
   public:
@@ -19,31 +19,31 @@ namespace dirko
   };
 }
 
-template < class T >
+template< class T >
 bool dirko::Queue< T >::empty() const noexcept
 {
   return !data_.size();
 }
 
-template < class T >
+template< class T >
 size_t dirko::Queue< T >::size() const noexcept
 {
   return data_.size();
 }
 
-template < class T >
+template< class T >
 void dirko::Queue< T >::push(const T &rhs)
 {
   data_.push_back(rhs);
 }
 
-template < class T >
+template< class T >
 T &dirko::Queue< T >::get() noexcept
 {
   return data_.head();
 }
 
-template < class T >
+template< class T >
 void dirko::Queue< T >::pop() noexcept
 {
   data_.pop_front();
