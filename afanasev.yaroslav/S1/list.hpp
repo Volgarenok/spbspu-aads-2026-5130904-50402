0c20676a84e95d2aecc1ff8b1ff0faf1c6a281c8
jar <Jarafan3@gmail.com>
2026-05-20 14:29:31 +0300

    Mega Fix

--- afanasev.yaroslav/S1/list.hpp
+++ afanasev.yaroslav/S1/list.hpp
@@ -9,10 +9,14 @@
 
 namespace afanasev
 {
-  template< class T > class Node;
-  template< class T > class List;
-  template< class T > class LIter;
-  template< class T > class LCIter;
+  template< class T >
+  class Node;
+  template< class T >
+  class List;
+  template< class T >
+  class LIter;
+  template< class T >
+  class LCIter;
 
   template< class T >
   class Node
@@ -51,44 +55,44 @@ namespace afanasev
     void swap(List & other) noexcept;
   };
 
-  template < class T >
+  template< class T >
   List< T >::List():
     fake_(new Node< T >{T(), nullptr}),
     size_(0)
   {}
 
-  template < class T >
+  template< class T >
   List< T >::~List()
   {
     clear();
     delete fake_;
   }
 
-  template < class T >
+  template< class T >
   LIter< T > List< T >::begin() noexcept
   {
     return LIter< T >{fake_->next_};
   }
 
-  template < class T >
+  template< class T >
   LCIter< T > List< T >::begin() const noexcept
   {
     return LCIter< T >{fake_->next_};
   }
 
-  template < class T >
+  template< class T >
   T List< T >::getValue(LIter< T > pos) noexcept
   {
     return pos.curr_->val_;
   }
 
-  template < class T >
+  template< class T >
   T List< T >::getValue(LCIter< T > pos) const noexcept
   {
     return pos.curr_->val_;
   }
 
-  template < class T >
+  template< class T >
   void List< T >::pushFront(const T & val)
   {
     Node< T > * new_node = new Node< T >{val, fake_->next_};
@@ -96,7 +100,7 @@ namespace afanasev
     ++size_;
   }
 
-  template < class T >
+  template< class T >
   T List< T >::popFront()
   {
     if (!size_)
@@ -111,7 +115,7 @@ namespace afanasev
     return value;
   }
 
-  template < class T >
+  template< class T >
   void List< T >::insert(const T & val, LIter< T > pos)
   {
     Node< T > * new_node = new Node< T >{val, pos.curr_->next_};
@@ -119,7 +123,7 @@ namespace afanasev
     ++size_;
   }
 
-  template < class T >
+  template< class T >
   void List< T >::deleteNext(LIter< T > pos) noexcept
   {
     Node< T > * tmp = pos.curr_->next_;
@@ -133,7 +137,7 @@ namespace afanasev
     size_--;
   }
 
-  template < class T >
+  template< class T >
   void List< T >::clear() noexcept
   {
     Node< T > * h = fake_->next_;
@@ -147,19 +151,19 @@ namespace afanasev
     size_ = 0;
   }
 
-  template < class T >
+  template< class T >
   size_t List< T >::size() const noexcept
   {
     return size_;
   }
 
 
-  template < class T >
-  List< T >::List(const List< T > & other) :
-    fake_(new Node< T >{T(), nullptr}),
-    size_(0)
+  template< class T >
+  List< T >::List(const List< T > & other):
+    List()
   {
-    Node< T > * this_curr = fake_;
+    List tmp;
+    Node< T > * this_curr = tmp.fake_;
     Node< T > * other_curr = other.fake_->next_;
     while (other_curr)
     {
@@ -167,12 +171,13 @@ namespace afanasev
       this_curr->next_ = new_node;
       this_curr = new_node;
       other_curr = other_curr->next_;
-      ++size_;
+      ++tmp.size_;
     }
+    swap(tmp);
   }
 
-  template < class T >
-  List< T >::List(List< T > && other) noexcept :
+  template< class T >
+  List< T >::List(List< T > && other) noexcept:
     fake_(other.fake_),
     size_(other.size_)
   {
@@ -180,35 +185,29 @@ namespace afanasev
     other.size_ = 0;
   }
 
-  template < class T >
+  template< class T >
   List< T > & List< T >::operator=(const List< T > & other)
   {
-    if (this != & other)
+    if (this != std::addressof(other))
     {
-      List tmp(other);
+      List< T > tmp(std::move(other));
       swap(tmp);
     }
-    return * this;
+    return *this;
   }
 
-  template < class T >
+  template< class T >
   List< T > & List< T >::operator=(List< T > && other) noexcept
   {
-    if (this != & other)
+    if (this != std::addressof(other))
     {
-      clear();
-      delete fake_;
-
-      fake_ = other.fake_;
-      size_ = other.size_;
-
-      other.fake_ = new Node< T >{T(), nullptr};
-      other.size_ = 0;
+      List< T > tmp(other);
+      swap(tmp);
     }
-    return * this;
+    return *this;
   }
 
-  template < class T >
+  template< class T >
   void List< T >::swap(List< T > & other) noexcept
   {
     std::swap(fake_, other.fake_);
@@ -216,7 +215,7 @@ namespace afanasev
   }
 
 
-  template < class T >
+  template< class T >
   class LIter
   {
     friend class List< T >;
@@ -232,50 +231,50 @@ namespace afanasev
     bool operator!=(const LIter< T > & other) const;
   };
 
-  template < class T >
+  template< class T >
   LIter< T >::LIter():
     curr_(nullptr)
   {}
 
-  template < class T >
+  template< class T >
   LIter< T >::LIter(Node< T > * p):
     curr_(p)
   {}
 
-  template < class T >
+  template< class T >
   T & LIter< T >::operator*() const
   {
     return curr_->val_;
   }
 
-  template < class T >
+  template< class T >
   LIter< T > & LIter< T >::operator++()
   {
     curr_ = curr_->next_;
     return * this;
   }
 
-  template < class T >
+  template< class T >
   LIter< T > & LIter< T >::operator++(int)
   {
     curr_ = curr_->next_;
     return * this;
   }
 
-  template < class T >
+  template< class T >
   bool LIter< T >::operator==(const LIter< T > & other) const
   {
     return curr_ == other.curr_;
   }
 
-  template < class T >
+  template< class T >
   bool LIter< T >::operator!=(const LIter< T > & other) const
   {
     return curr_ != other.curr_;
   }
 
 
-  template < class T >
+  template< class T >
   class LCIter
   {
     friend class List< T >;
@@ -290,38 +289,38 @@ namespace afanasev
     bool operator!=(const LCIter< T > & other) const;
   };
 
-  template < class T >
+  template< class T >
   LCIter< T >::LCIter(const Node< T > * p):
     curr_(p)
   {}
 
-  template < class T >
+  template< class T >
   const T & LCIter< T >::operator*() const
   {
     return curr_->val_;
   }
 
-  template < class T >
+  template< class T >
   LCIter< T > & LCIter< T >::operator++()
   {
     curr_ = curr_->next_;
     return * this;
   }
 
-  template < class T >
+  template< class T >
   LCIter< T > & LCIter< T >::operator++(int)
   {
     curr_ = curr_->next_;
     return * this;
   }
 
-  template < class T >
+  template< class T >
   bool LCIter< T >::operator==(const LCIter< T > & other) const
   {
     return curr_ == other.curr_;
   }
 
-  template < class T >
+  template< class T >
   bool LCIter< T >::operator!=(const LCIter< T > & other) const
   {
     return curr_ != other.curr_;
